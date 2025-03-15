9b1ee6b1010b35131126c7c85b7332bde1b16eef
Pratyush Joshi <dmodeler@gmail.com>
2025-03-14 23:44:57 -0400

    Added SQL Server solution for Database - Compute the rank as a percentage

--- database/medium/compute_the_rank_as_a_percentage.sql
+++ database/medium/compute_the_rank_as_a_percentage.sql
@@ -5,6 +5,23 @@
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t as (
+select student_id, department_id, mark,
+       rank() over (partition by department_id order by mark desc) as rnk,
+       count(*) over (partition by department_id) as student_count
+from students
+)
+select student_id, department_id,
+       round( case when student_count = 1 then 0
+                   else convert(float, ((rnk - 1) * 100) )
+                        /
+                        convert(float, (student_count - 1) )
+              end,
+              2
+            ) as percentage
+from t
+;
 
 
 # MySQL
