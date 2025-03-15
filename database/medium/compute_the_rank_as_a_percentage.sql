ea8800712582439ee3ce6a0c9209c270d32a9c38
Pratyush Joshi <dmodeler@gmail.com>
2025-03-14 23:46:21 -0400

    Added Oracle solution for Database - Compute the rank as a percentage

--- database/medium/compute_the_rank_as_a_percentage.sql
+++ database/medium/compute_the_rank_as_a_percentage.sql
@@ -1,4 +1,19 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+with t as (
+select student_id, department_id, mark,
+       rank() over (partition by department_id order by mark desc) as rnk,
+       count(*) over (partition by department_id) as student_count
+from students
+)
+select student_id, department_id,
+       round( case when student_count = 1 then 0
+                   else ((rnk - 1) * 100) / (student_count - 1)
+              end,
+              2
+            ) as percentage
+from t
+;
 
 
 -- PostgreSQL
