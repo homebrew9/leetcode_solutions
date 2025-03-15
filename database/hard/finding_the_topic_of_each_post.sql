b00ea31b659e3a84c3a3967fbec3e244c5285d93
Pratyush Joshi <dmodeler@gmail.com>
2025-03-14 23:28:43 -0400

    Added SQL Server solution for Database - Finding the topic of each post

--- database/hard/finding_the_topic_of_each_post.sql
+++ database/hard/finding_the_topic_of_each_post.sql
@@ -34,6 +34,24 @@ where post_id not in (select distinct post_id from t)
 ;
 
 
+-- SQL Server
+/* Write your T-SQL query statement below */
+with t (post_id, topic_id) as (
+    select distinct p.post_id, k.topic_id
+    from posts p
+        cross apply string_split(p.content, ' ') ss
+        inner join keywords k on (lower(k.word) = lower(ss.value))
+)
+select post_id, string_agg(topic_id, ',') within group (order by topic_id) as topic
+  from t
+ group by post_id
+union all
+select p.post_id, 'Ambiguous!' as topic
+  from posts p
+ where not exists (select null from t where t.post_id = p.post_id)
+;
+
+
 # MySQL
 # Write your MySQL query statement below
 with t as (
@@ -52,4 +70,7 @@ select post_id, 'Ambiguous!'
 from posts
 where post_id not in (select distinct post_id from t)
 ;
+
+
+# Pandas
 
