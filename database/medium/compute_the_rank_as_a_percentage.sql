aacc3eee825909dd287802880071b103f757028e
Pratyush Joshi <dmodeler@gmail.com>
2025-03-14 23:45:43 -0400

    Added PostgreSQL solution for Database - Compute the rank as a percentage

--- database/medium/compute_the_rank_as_a_percentage.sql
+++ database/medium/compute_the_rank_as_a_percentage.sql
@@ -2,6 +2,21 @@
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t as (
+select student_id, department_id, mark,
+       rank() over (partition by department_id order by mark desc) as rnk,
+       count(*) over (partition by department_id) as student_count
+from students
+)
+select student_id, department_id,
+       round( case when student_count = 1 then 0
+                   else ((rnk - 1) * 100)::numeric / (student_count - 1)::numeric
+              end,
+              2
+            ) as percentage
+from t
+;
 
 
 -- SQL Server
