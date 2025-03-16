65f3061c36e27014ebb9e39703e1651dbcd8563d
Pratyush Joshi <dmodeler@gmail.com>
2025-03-16 19:19:49 -0400

    Added Pandas solution for Database - First letter capitalization II

--- database/hard/first_letter_capitalization_2.sql
+++ database/hard/first_letter_capitalization_2.sql
@@ -71,4 +71,9 @@ group by content_id, content_text
 
 
 # Pandas
+import pandas as pd
+
+def capitalize_content(user_content: pd.DataFrame) -> pd.DataFrame:
+    user_content['converted_text'] = user_content['content_text'].str.title()
+    return user_content.rename(columns={'content_text': 'original_text'})
 
