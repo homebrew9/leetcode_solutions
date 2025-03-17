8cb99d6e3e0630f49edfc7cd3ee1da8a34b7ca48
Pratyush Joshi <dmodeler@gmail.com>
2025-03-17 18:29:41 -0400

    Added PostgreSQL version 1 solution for Database - Market analysis III

--- database/medium/market_analysis_3.sql
+++ database/medium/market_analysis_3.sql
@@ -23,7 +23,28 @@ order by x.seller_id
 
 
 -- PostgreSQL
-
+-- Version 1 - Using DENSE_RANK analytic function
+with t (seller_id, num_items) as (
+    select o.seller_id, count(distinct o.item_id) as num_items
+      from orders o
+           inner join items i on (i.item_id = o.item_id)
+     where not exists (select null
+                         from users u
+                        where u.seller_id = o.seller_id
+                          and u.favorite_brand = i.item_brand
+                      )
+     group by o.seller_id
+),
+t1 as (
+    select seller_id, num_items,
+           dense_rank() over (order by num_items desc) as drnk
+      from t
+)
+select seller_id, num_items
+  from t1
+ where drnk = 1
+ order by seller_id
+;
 
 -- SQL Server
 /* Write your T-SQL query statement below */
