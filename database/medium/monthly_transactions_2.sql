e1a0e8b99e81173daf992192a463655425e15364
Pratyush Joshi <dmodeler@gmail.com>
2025-03-18 10:42:13 -0400

    Added solution for Database - Monthly transactions II

---
+++ database/medium/monthly_transactions_2.sql
@@ -0,0 +1,142 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+with t_approved(month, country, approved_count, approved_amount) as (
+    select to_char(trans_date, 'YYYY-MM') as month, country,
+           sum(case state when 'approved' then 1 else 0 end) as approved_count,
+           sum(case state when 'approved' then amount else 0 end) as approved_amount
+      from transactions
+     group by to_char(trans_date, 'YYYY-MM'), country
+),
+t_chargeback (month, country, chargeback_count, chargeback_amount) as (
+    select to_char(c.trans_date, 'YYYY-MM') as month, t.country,
+           count(*) as chargeback_count,
+           sum(t.amount) as chargeback_amount
+      from chargebacks c
+           inner join transactions t on (t.id = c.trans_id)
+     group by to_char(c.trans_date, 'YYYY-MM'), t.country
+)
+select coalesce(a.month, c.month) as month,
+       coalesce(a.country, c.country) as country,
+       coalesce(a.approved_count, 0) as approved_count,
+       coalesce(a.approved_amount, 0) as approved_amount,
+       coalesce(c.chargeback_count, 0) as chargeback_count,
+       coalesce(c.chargeback_amount, 0) as chargeback_amount
+from t_approved a
+     full outer join t_chargeback c on (a.country = c.country and a.month = c.month)
+where not (
+       coalesce(a.approved_count, 0) = 0 and
+       coalesce(a.approved_amount, 0) = 0 and
+       coalesce(c.chargeback_count, 0) = 0 and
+       coalesce(c.chargeback_amount, 0) = 0
+)
+;
+
+
+-- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t_approved(month, country, approved_count, approved_amount) as (
+    select to_char(trans_date, 'YYYY-MM') as month, country,
+           sum(case state when 'approved' then 1 else 0 end) as approved_count,
+           sum(case state when 'approved' then amount else 0 end) as approved_amount
+      from transactions
+     group by to_char(trans_date, 'YYYY-MM'), country
+),
+t_chargeback (month, country, chargeback_count, chargeback_amount) as (
+    select to_char(c.trans_date, 'YYYY-MM') as month, t.country,
+           count(*) as chargeback_count,
+           sum(t.amount) as chargeback_amount
+      from chargebacks c
+           inner join transactions t on (t.id = c.trans_id)
+     group by to_char(c.trans_date, 'YYYY-MM'), t.country
+)
+select coalesce(a.month, c.month) as month,
+       coalesce(a.country, c.country) as country,
+       coalesce(a.approved_count, 0) as approved_count,
+       coalesce(a.approved_amount, 0) as approved_amount,
+       coalesce(c.chargeback_count, 0) as chargeback_count,
+       coalesce(c.chargeback_amount, 0) as chargeback_amount
+from t_approved a
+     full outer join t_chargeback c on (a.country = c.country and a.month = c.month)
+where not (
+       coalesce(a.approved_count, 0) = 0 and
+       coalesce(a.approved_amount, 0) = 0 and
+       coalesce(c.chargeback_count, 0) = 0 and
+       coalesce(c.chargeback_amount, 0) = 0
+)
+;
+
+
+-- SQL Server
+/* Write your T-SQL query statement below */
+with t_approved(month, country, approved_count, approved_amount) as (
+    select left(convert(varchar, trans_date, 23), 7) as month, country,
+           sum(case state when 'approved' then 1 else 0 end) as approved_count,
+           sum(case state when 'approved' then amount else 0 end) as approved_amount
+      from transactions
+     group by left(convert(varchar, trans_date, 23), 7), country
+),
+t_chargeback (month, country, chargeback_count, chargeback_amount) as (
+    select left(convert(varchar, c.trans_date, 23), 7) as month, t.country,
+           count(*) as chargeback_count,
+           sum(t.amount) as chargeback_amount
+      from chargebacks c
+           inner join transactions t on (t.id = c.trans_id)
+     group by left(convert(varchar, c.trans_date, 23), 7), t.country
+)
+select coalesce(a.month, c.month) as month,
+       coalesce(a.country, c.country) as country,
+       coalesce(a.approved_count, 0) as approved_count,
+       coalesce(a.approved_amount, 0) as approved_amount,
+       coalesce(c.chargeback_count, 0) as chargeback_count,
+       coalesce(c.chargeback_amount, 0) as chargeback_amount
+from t_approved a
+     full outer join t_chargeback c on (a.country = c.country and a.month = c.month)
+where not (
+       coalesce(a.approved_count, 0) = 0 and
+       coalesce(a.approved_amount, 0) = 0 and
+       coalesce(c.chargeback_count, 0) = 0 and
+       coalesce(c.chargeback_amount, 0) = 0
+)
+;
+
+
+# MySQL
+# Write your MySQL query statement below
+# MySQL 8.0 does not support FULL OUTER JOINS!!!
+with t_approved(month, country, approved_count, approved_amount) as (
+    select date_format(trans_date, '%Y-%m') as month, country,
+           sum(case state when 'approved' then 1 else 0 end) as approved_count,
+           sum(case state when 'approved' then amount else 0 end) as approved_amount
+      from transactions
+     group by date_format(trans_date, '%Y-%m'), country
+),
+t_chargeback (month, country, chargeback_count, chargeback_amount) as (
+    select date_format(c.trans_date, '%Y-%m') as month, t.country,
+           count(*) as chargeback_count,
+           sum(t.amount) as chargeback_amount
+      from chargebacks c
+           inner join transactions t on (t.id = c.trans_id)
+     group by date_format(c.trans_date, '%Y-%m'), t.country
+),
+t_consolidated (month, country, approved_count, approved_amount, chargeback_count, chargeback_amount) as (
+select a.month, a.country, a.approved_count, a.approved_amount,
+       coalesce(c.chargeback_count, 0) as chargeback_count,
+       coalesce(c.chargeback_amount, 0) as chargeback_amount
+from t_approved a
+     left outer join t_chargeback c on (c.country = a.country and c.month = a.month)
+union
+select c.month, c.country,
+       coalesce(a.approved_count, 0) as approved_count,
+       coalesce(a.approved_amount, 0) as approved_amount,
+       c.chargeback_count, c.chargeback_amount
+from t_approved a
+     right outer join t_chargeback c on (c.country = a.country and c.month = a.month)
+)
+select month, country, approved_count, approved_amount, chargeback_count, chargeback_amount
+from t_consolidated
+where not (approved_count=0 and approved_amount=0 and chargeback_count=0 and chargeback_amount=0)
+;
+
+
+# Pandas
+
