a8af069c1acdd843d1ac9da06aefbb2030fc4e21
Pratyush Joshi <dmodeler@gmail.com>
2025-03-19 20:02:51 -0400

    Added Pandas solution for Database - Find candidates for data scientist position II

--- database/medium/find_candidates_for_data_scientist_position_2.sql
+++ database/medium/find_candidates_for_data_scientist_position_2.sql
@@ -187,4 +187,34 @@ select project_id, candidate_id, score
 
 
 # Pandas
+import pandas as pd
+
+def find_best_candidates(candidates: pd.DataFrame, projects: pd.DataFrame) -> pd.DataFrame:
+    # Cross join projects with all distinct candidates, and then left join with candidates
+    # on candidate_id and skill.
+    df = ( projects
+          .merge(candidates[['candidate_id']].drop_duplicates(), how='cross')
+          .merge(candidates, how='left', on=['candidate_id','skill'])
+         )
+    
+    # If a candidate proficiency is NULL, then that candidate does not have all skills for
+    # the project. We need to remove all such (project_id, candidate_id) tuples.
+    df1 = df[df['proficiency'].isna()][['project_id','candidate_id']]
+    
+    # A hack to compare .isin() with multiple columns!
+    df1['indicator'] = df1['project_id'].astype(str) + '#' + df1['candidate_id'].astype(str)
+    df = df[~(df['project_id'].astype(str)+'#'+df['candidate_id'].astype(str)).isin(df1['indicator'])]
+    
+    # Now assign the score for all suitable candidates
+    df['score'] = np.where(df['proficiency'] > df['importance'], 10, np.where(df['proficiency'] < df['importance'], -5, 0))
+    df = df.groupby(['project_id','candidate_id'], as_index=0)['score'].sum()
+    df['score'] = df['score'] + 100
+    
+    # Rank by highest to lowest score and for each such rank, do further ranking on candidate_id ascending
+    df['srnk'] = df.groupby('project_id', as_index=0)['score'].rank(method='dense', ascending=False)
+    return ( df
+            .assign(crnk=df.groupby(['project_id','srnk'], as_index=0)['candidate_id'].rank(method='dense', ascending=True))
+            .query('srnk == 1 & crnk == 1')[['project_id','candidate_id','score']]
+            .sort_values('project_id')
+           )
 
