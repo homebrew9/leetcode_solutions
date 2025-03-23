2e611ae021df077fcc5a955609ba20d575e18875
Pratyush Joshi <dmodeler@gmail.com>
2025-03-23 17:13:38 -0400

    Added Pandas solution for Database - The number of passengers in each bus I

--- database/medium/the_number_of_passengers_in_each_bus_1.sql
+++ database/medium/the_number_of_passengers_in_each_bus_1.sql
@@ -62,4 +62,22 @@ order by bus_id
 
 
 # Pandas
+import pandas as pd
+
+def count_passengers_in_bus(buses: pd.DataFrame, passengers: pd.DataFrame) -> pd.DataFrame:
+    buses.sort_values('arrival_time', inplace=True)
+    buses['prev_arrival_time'] = buses['arrival_time'].shift(1).fillna(-1)
+    df = ( buses
+          .merge(passengers, how='cross')
+          .query('arrival_time_y > prev_arrival_time and arrival_time_y <= arrival_time_x')
+          .groupby('bus_id', as_index=0)['passenger_id']
+          .count()
+          .rename(columns={'passenger_id': 'passengers_cnt'})
+         )
+    return ( buses[['bus_id']]
+            .drop_duplicates()
+            .merge(df, how='left', on='bus_id')
+            .fillna(0)
+            .sort_values('bus_id')
+           )
 
