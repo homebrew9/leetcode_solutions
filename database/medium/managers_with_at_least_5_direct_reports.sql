7ea4d3af562cd6e2b2ba93041344748778086ab7
Pratyush Joshi <dmodeler@gmail.com>
2025-03-22 22:07:17 -0400

    Added Pandas solution for Database - Managers with at least 5 direct reports

--- database/medium/managers_with_at_least_5_direct_reports.sql
+++ database/medium/managers_with_at_least_5_direct_reports.sql
@@ -39,4 +39,9 @@ having count(*) >= 5
 
 
 # Pandas
+import pandas as pd
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.groupby('managerId', as_index=False).agg(reporting=('id','count')).query('reporting >= 5')['managerId']
+    return employee[employee['id'].isin(df)][['name']]
 
