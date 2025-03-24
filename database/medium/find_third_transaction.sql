5bcc2a4a5d6d7d10a4d0a63e3bea99636bd22689
Pratyush Joshi <dmodeler@gmail.com>
2025-03-24 11:22:21 -0400

    Added solution for Database - Find third transaction

---
+++ database/medium/find_third_transaction.sql
@@ -0,0 +1,78 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+with t (user_id, spend, transaction_date, trx_no, prev_trx_1, prev_trx_2) as (
+select user_id, spend, transaction_date,
+       row_number() over (partition by user_id order by transaction_date) as trx_no,
+       lag(spend) over (partition by user_id order by transaction_date) as prev_trx_1,
+       lag(spend, 2) over (partition by user_id order by transaction_date) as prev_trx_2
+from transactions
+)
+select user_id,
+       spend as third_transaction_spend,
+       transaction_date as third_transaction_date
+from t
+where trx_no = 3
+and spend > prev_trx_1
+and spend > prev_trx_2
+;
+
+
+-- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t (user_id, spend, transaction_date, trx_no, prev_trx_1, prev_trx_2) as (
+select user_id, spend, transaction_date,
+       row_number() over (partition by user_id order by transaction_date) as trx_no,
+       lag(spend) over (partition by user_id order by transaction_date) as prev_trx_1,
+       lag(spend, 2) over (partition by user_id order by transaction_date) as prev_trx_2
+from transactions
+)
+select user_id,
+       spend as third_transaction_spend,
+       transaction_date as third_transaction_date
+from t
+where trx_no = 3
+and spend > prev_trx_1
+and spend > prev_trx_2
+;
+
+
+-- SQL Server
+/* Write your T-SQL query statement below */
+with t (user_id, spend, transaction_date, trx_no, prev_trx_1, prev_trx_2) as (
+select user_id, spend, transaction_date,
+       row_number() over (partition by user_id order by transaction_date) as trx_no,
+       lag(spend) over (partition by user_id order by transaction_date) as prev_trx_1,
+       lag(spend, 2) over (partition by user_id order by transaction_date) as prev_trx_2
+from transactions
+)
+select user_id,
+       spend as third_transaction_spend,
+       transaction_date as third_transaction_date
+from t
+where trx_no = 3
+and spend > prev_trx_1
+and spend > prev_trx_2
+;
+
+
+# MySQL
+# Write your MySQL query statement below
+with t (user_id, spend, transaction_date, trx_no, prev_trx_1, prev_trx_2) as (
+select user_id, spend, transaction_date,
+       row_number() over (partition by user_id order by transaction_date) as trx_no,
+       lag(spend) over (partition by user_id order by transaction_date) as prev_trx_1,
+       lag(spend, 2) over (partition by user_id order by transaction_date) as prev_trx_2
+from transactions
+)
+select user_id,
+       spend as third_transaction_spend,
+       transaction_date as third_transaction_date
+from t
+where trx_no = 3
+and spend > prev_trx_1
+and spend > prev_trx_2
+;
+
+
+# Pandas
+
