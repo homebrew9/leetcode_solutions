e83c29c1c0ee6fee3652a4eea0394c044dac71f3
Pratyush Joshi <dmodeler@gmail.com>
2025-03-24 11:18:16 -0400

    Added Pandas solution for Database - Calculate parking fees and duration

--- database/medium/calculate_parking_fees_and_duration.sql
+++ database/medium/calculate_parking_fees_and_duration.sql
@@ -129,4 +129,39 @@ order by t.car_id
 
 
 # Pandas
+import pandas as pd
+
+def calculate_fees_and_duration(parking_transactions: pd.DataFrame) -> pd.DataFrame:
+    parking_transactions['hours'] = ( (parking_transactions['exit_time']
+                                       - parking_transactions['entry_time']
+                                      ).dt.seconds/60/60
+                                    )
+    df = ( parking_transactions
+          .groupby(['car_id','lot_id'],as_index=0)['hours']
+          .sum()
+          .sort_values(by=['car_id','hours'],ascending=[True,False])
+         )
+    df_most_time_lot = ( df
+                        .assign(
+                                 drnk= df.groupby('car_id',as_index=0)['hours']
+                                         .rank(method='dense', ascending=False)
+                               )
+                        .query('drnk == 1')[['car_id','lot_id']]
+                       )
+    df_most_time_lot.rename(columns={'lot_id': 'most_time_lot'}, inplace=True)
+    df_fees = ( parking_transactions
+               .groupby('car_id',as_index=0)
+               .agg({'fee_paid':'sum', 'hours':'sum'})
+              )
+    df_fees['avg_hourly_fee'] = round(df_fees['fee_paid'] / df_fees['hours'], 2)
+    df_fees.rename(columns={'fee_paid': 'total_fee_paid'},inplace=True)
+    return ( df_fees
+            .merge(df_most_time_lot, how='inner', on='car_id')[[
+                'car_id',
+                'total_fee_paid',
+                'avg_hourly_fee',
+                'most_time_lot'
+            ]]
+            .sort_values(['car_id'])
+           )
 
