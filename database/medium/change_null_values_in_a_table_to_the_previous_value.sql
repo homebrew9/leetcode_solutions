fba04f0fbf03eb244797fe1ed985bd954ed9dea4
Pratyush Joshi <dmodeler@gmail.com>
2025-03-24 12:17:12 -0400

    Added SQL Server solution for Database - Change null values in a table to the previous value

--- database/medium/change_null_values_in_a_table_to_the_previous_value.sql
+++ database/medium/change_null_values_in_a_table_to_the_previous_value.sql
@@ -30,6 +30,22 @@ select c.id, coalesce(c.drink, t.drink) as drink
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t (id, drink, rnum) as (
+    select id, drink, row_number() over (order by (select null)) as rnum
+      from coffeeshop
+),
+consolidated (id, drink, rnum, max_t2_rnum) as (
+    select t1.id, t1.drink, t1.rnum, max(t2.rnum) as max_t2_rnum
+      from t t1
+             left outer join t t2 on (t2.rnum < t1.rnum and t2.drink is not null)
+     group by t1.id, t1.drink, t1.rnum
+)
+select c.id, coalesce(c.drink, t.drink) as drink
+  from consolidated c
+       left outer join t on (t.rnum = c.max_t2_rnum)
+ order by c.rnum
+;
 
 
 # MySQL
