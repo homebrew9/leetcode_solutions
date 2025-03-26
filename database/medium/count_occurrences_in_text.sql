ee17d44323e0b255b72d76f8fe4fe474f802b0d8
Pratyush Joshi <dmodeler@gmail.com>
2025-03-26 19:40:25 -0400

    Added solution for Database - Count occurrences in text

--- database/medium/count_occurrences_in_text.sql
+++ database/medium/count_occurrences_in_text.sql
@@ -7,13 +7,34 @@ select 'bear' as word, (select count(*) from files where regexp_like(content, '\
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+select 'bull' as word, (select count(*) from files where regexp_like(content, '\sbull\s')) as count
+union all
+select 'bear' as word, (select count(*) from files where regexp_like(content, '\sbear\s')) as count
+;
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select 'bull' as word, (select count(*) from files where content like '% bull %') as count
+union all
+select 'bear' as word, (select count(*) from files where content like '% bear %') as count
+;
 
 
 # MySQL
+# Write your MySQL query statement below
+select 'bull' as word, (select count(*) from files where content LIKE '% bull %') as count
+union all
+select 'bear' as word, (select count(*) from files where content LIKE '% bear %') as count
+;
 
 
 # Pandas
+import pandas as pd
+
+def count_occurrences(files: pd.DataFrame) -> pd.DataFrame:
+    bull_count = len(files[files['content'].str.contains('^.* bull .*$')])
+    bear_count = len(files[files['content'].str.contains('^.* bear .*$')])
+    return pd.DataFrame(data={'word': ['bull','bear'], 'count': [bull_count, bear_count]})
 
