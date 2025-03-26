24ccf42b7796c7a55c87d30fb5d3178630e182ce
Pratyush Joshi <dmodeler@gmail.com>
2025-03-25 23:44:15 -0400

    Added SQL Server solution for Database - Department highest salary

--- database/medium/department_highest_salary.sql
+++ database/medium/department_highest_salary.sql
@@ -5,6 +5,15 @@
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select d.name as department, e.name as employee, e.salary
+  from employee e
+       inner join department d on (d.id = e.departmentId)
+ where e.salary = (select max(e1.salary)
+                     from employee e1
+                    where e1.departmentId = e.departmentId
+                  )
+;
 
 
 # MySQL
