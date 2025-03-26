62ea5d8b0632c2b25d567f0045613d9e8c7378df
Pratyush Joshi <dmodeler@gmail.com>
2025-03-25 23:48:31 -0400

    Added SQL Server solution for Database - Strong friendship

--- database/medium/strong_friendship.sql
+++ database/medium/strong_friendship.sql
@@ -5,6 +5,20 @@
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t (user_id, friend_id) as (
+    select user1_id, user2_id from friendship
+    union
+    select user2_id, user1_id from friendship
+)
+select f.user1_id, f.user2_id,
+       count(*) as common_friend
+from friendship f
+     inner join t t1 on (t1.user_id = f.user1_id)
+     inner join t t2 on (t2.user_id = f.user2_id and t2.friend_id = t1.friend_id)
+group by f.user1_id, f.user2_id
+having count(*) >= 3
+;
 
 
 # MySQL
