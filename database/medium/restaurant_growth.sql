c8feecb7720399f5c51538eaf3019417b681ac5c
Pratyush Joshi <dmodeler@gmail.com>
2025-03-26 19:58:05 -0400

    Added solution for Database - Restaurant growth

---
+++ database/medium/restaurant_growth.sql
@@ -0,0 +1,35 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+with t (visited_on, amount) as (
+    select visited_on, sum(amount) as amount
+      from Customer
+     group by visited_on
+),
+t1 (visited_on, amount, rnum) as (
+    select visited_on, amount, row_number() over (order by visited_on) as rnum
+      from t
+),
+t2 (visited_on, rnum, amount, average_amount) as (
+select to_char(visited_on, 'YYYY-MM-DD') as visited_on,
+       rnum,
+       round(sum(amount) over (order by visited_on rows between 6 preceding and current row),2) as amount,
+       round(avg(amount) over (order by visited_on rows between 6 preceding and current row),2) as average_amount
+  from t1
+)
+select visited_on, amount, average_amount
+from t2
+where rnum >= 7
+;
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+
