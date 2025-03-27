b41d03d825dcf7ea04b440615362150b1b4d92f1
Pratyush Joshi <dmodeler@gmail.com>
2025-03-26 20:05:36 -0400

    Added SQL Server solution for Database - Friday purchase III

--- database/medium/friday_purchase_3.sql
+++ database/medium/friday_purchase_3.sql
@@ -57,6 +57,35 @@ order by t2.week_of_month, t2.membership
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t (dt, num) as (
+    select convert(date, '2023-11-01') as dt, 0 as num
+    union all
+    select dateadd(day, 1, t.dt), t.num + 1
+      from t
+     where t.num + 1 < 30
+),
+t1 (dt, week_of_month, membership) as (
+    select dt,
+           datepart(week, dt) - datepart(week, '2023-11-01') + 1 as week_of_month,
+           m.membership
+      from t
+           cross join (select 'Premium' as membership union all
+                       select 'VIP'
+                      ) m
+     where datename(weekday, dt) = 'Friday'
+),
+consolidated (purchase_date, membership, total_amount) as (
+    select p.purchase_date, u.membership, sum(p.amount_spend) as total_amount
+      from purchases p
+           inner join users u on (u.user_id = p.user_id)
+     group by p.purchase_date, u.membership
+)
+select t1.week_of_month, t1.membership, coalesce(c.total_amount, 0) as total_amount
+  from t1
+       left outer join consolidated c on (c.purchase_date = t1.dt and c.membership = t1.membership)
+order by t1.week_of_month, t1.membership
+;
 
 
 # MySQL
