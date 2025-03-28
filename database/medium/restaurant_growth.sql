5ab4eb030c21dfeccb0b81fe2ecd0195d05fb26f
Pratyush Joshi <dmodeler@gmail.com>
2025-03-27 22:26:55 -0400

    Added MySQL solution for Restaurant growth

--- database/medium/restaurant_growth.sql
+++ database/medium/restaurant_growth.sql
@@ -71,7 +71,27 @@ select visited_on, amount, average_amount
 
 
 # MySQL
-
+# Write your MySQL query statement below
+with t (visited_on, amount) as (
+    select visited_on, sum(amount) as amount
+      from customer
+     group by visited_on
+),
+t1 (visited_on, amount, rnum) as (
+    select visited_on, amount, row_number() over (order by visited_on) as rnum
+      from t
+),
+t2 (visited_on, rnum, amount, average_amount) as (
+    select visited_on, rnum,
+           round(sum(amount) over (order by rnum rows between 6 preceding and current row), 2) as amount,
+           round(avg(amount) over (order by rnum rows between 6 preceding and current row), 2) as average_amount
+      from t1
+)
+select visited_on, amount, average_amount
+  from t2
+ where rnum >= 7
+ order by rnum
+;
 
 # Pandas
 
