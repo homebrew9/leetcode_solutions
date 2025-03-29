ab18d2e5fff128b3e6d0f845962852d57c9f70c5
Pratyush Joshi <dmodeler@gmail.com>
2025-03-29 01:01:19 -0400

    Added PostgreSQL solution for Database - Market analysis I

--- database/medium/market_analysis_1.sql
+++ database/medium/market_analysis_1.sql
@@ -15,6 +15,13 @@ group by u.user_id, to_char(u.join_date, 'YYYY-MM-DD')
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+select u.user_id as buyer_id, u.join_date, coalesce(count(o.order_id), 0) as orders_in_2019
+  from users u
+       left outer join orders o on (o.buyer_id = u.user_id and to_char(o.order_date, 'yyyy') = '2019')
+  group by u.user_id, u.join_date
+ order by u.user_id
+;
 
 
 -- SQL Server
