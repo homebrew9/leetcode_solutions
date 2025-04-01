360648f705f44175914109b20fa59794cc84e654
Pratyush Joshi <dmodeler@gmail.com>
2025-04-01 19:23:05 -0400

    Added Pandas solution for Database - Top percentile fraud

--- database/medium/top_percentile_fraud.sql
+++ database/medium/top_percentile_fraud.sql
@@ -67,4 +67,16 @@ ORDER BY state, fraud_score DESC, policy_id
 
 
 # Pandas
+import pandas as pd
 
+def top_percentile_fraud(fraud: pd.DataFrame) -> pd.DataFrame:
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.core.groupby.DataFrameGroupBy.quantile.html#pandas.core.groupby.DataFrameGroupBy.quantile
+    # https://numpy.org/doc/stable/reference/generated/numpy.percentile.html#numpy.percentile
+    # Group the data by state and calculate the top 5 percentile
+    state_quantiles = fraud.groupby('state')['fraud_score'].quantile(0.95).reset_index(name='top_5_percentile')
+    # Merge the percentiles back to the fraud DataFrame
+    fraud = fraud.merge(state_quantiles, how='inner', on='state')
+    # Trim the fraud DataFrame to the top 5 percentile data only
+    fraud = fraud[fraud['fraud_score'] >= fraud['top_5_percentile']][['policy_id','state','fraud_score']]
+    # Return data in the correct sorted order
+    return fraud.sort_values(by=['state','fraud_score','policy_id'],ascending=[1,0,1])
