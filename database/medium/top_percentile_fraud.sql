6ede0b09a9656e277aad5eb411e198bcc035206a
Pratyush Joshi <dmodeler@gmail.com>
2025-04-01 19:21:54 -0400

    Added MySQL solution for Database - Top percentile fraud

--- database/medium/top_percentile_fraud.sql
+++ database/medium/top_percentile_fraud.sql
@@ -50,7 +50,20 @@ ORDER BY state, fraud_score DESC, policy_id
 
 
 # MySQL
-
+# Write your MySQL query statement below
+WITH t AS (
+select f1.policy_id, f1.state, f1.fraud_score,
+       (SELECT count(*) FROM fraud f2 WHERE f2.state = f1.state AND f2.fraud_score > f1.fraud_score) AS cnt_higher_scores,
+       (SELECT count(*) FROM fraud f2 WHERE f2.state = f1.state) AS total_scores,
+       (SELECT count(*) FROM fraud f2 WHERE f2.state = f1.state AND f2.fraud_score > f1.fraud_score) /
+       (SELECT count(*) FROM fraud f2 WHERE f2.state = f1.state) AS top_percentile_score
+from fraud f1
+)
+SELECT policy_id, state, fraud_score
+FROM t
+WHERE t.top_percentile_score <= 0.05
+ORDER BY state, fraud_score DESC, policy_id
+;
 
 # Pandas
 
