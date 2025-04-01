7d590d1a9bcc96cd25441dd1230ecc62711cb4eb
Pratyush Joshi <dmodeler@gmail.com>
2025-03-31 23:27:51 -0400

    Added Pandas solution for Database - Investments in 2016

--- database/medium/investments_in_2016.sql
+++ database/medium/investments_in_2016.sql
@@ -35,4 +35,21 @@ select round(sum(x.tiv_2016), 2) as tiv_2016
 
 
 # Pandas
+import pandas as pd
+
+def find_investments(insurance: pd.DataFrame) -> pd.DataFrame:
+    df = ( insurance
+          .merge(insurance, on='tiv_2015')
+          .query('pid_x != pid_y')[['pid_x','lat_x','lon_x','tiv_2016_x']]
+          .drop_duplicates()
+          .rename(columns={'pid_x':'pid','lat_x':'lat','lon_x':'lon','tiv_2016_x':'tiv_2016'})
+         )
+    invalid_pid_list = ( df
+                        .merge(insurance, how='inner', left_on=['lat','lon'], right_on=['lat','lon'])
+                        .query('pid_x != pid_y')[['pid_x']]
+                        .drop_duplicates()
+                        .rename(columns={'pid_x':'pid'})
+                       )
+    tiv_2016_sum = round(df[~df['pid'].isin(invalid_pid_list['pid'])]['tiv_2016'].sum(), 2)
+    return pd.DataFrame(data={'tiv_2016': [tiv_2016_sum]})
 
