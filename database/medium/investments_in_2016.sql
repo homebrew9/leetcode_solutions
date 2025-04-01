871d1ca4cbdb1eb7f2981ee79f6c05f91c7c91fc
Pratyush Joshi <dmodeler@gmail.com>
2025-03-31 23:25:49 -0400

    Added SQL Server solution for Database - Investments in 2016

--- database/medium/investments_in_2016.sql
+++ database/medium/investments_in_2016.sql
@@ -17,7 +17,12 @@ select round(sum(x.tiv_2016)::numeric, 2) as tiv_2016
 
 
 -- SQL Server
-
+/* Write your T-SQL query statement below */
+select round(convert(float, sum(x.tiv_2016)), 2) as tiv_2016
+  from Insurance x
+ where exists (select null from Insurance y where y.pid != x.pid and y.tiv_2015 = x.tiv_2015)
+   and not exists (select null from Insurance z where z.pid != x.pid and z.lat = x.lat and z.lon = x.lon)
+;
 
 # MySQL
 
