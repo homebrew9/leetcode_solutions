3b02b7b697243202b2088ce99fa7d630f8ec3805
Pratyush Joshi <dmodeler@gmail.com>
2025-03-31 23:22:58 -0400

    Added Pandas solution for Database - League statistics

--- database/medium/league_statistics.sql
+++ database/medium/league_statistics.sql
@@ -123,4 +123,37 @@ order by points desc, goal_diff desc, team_name
 
 
 # Pandas
+import pandas as pd
+
+def league_statistics(teams: pd.DataFrame, matches: pd.DataFrame) -> pd.DataFrame:
+    matches['home_team_points'] = ( np.where(matches['home_team_goals'] > matches['away_team_goals'],
+                                    3,
+                                    np.where(matches['home_team_goals'] < matches['away_team_goals'], 0, 1))
+                                  )
+    matches['away_team_points'] = ( np.where(matches['away_team_goals'] > matches['home_team_goals'],
+                                    3,
+                                    np.where(matches['away_team_goals'] < matches['home_team_goals'], 0, 1))
+                                  )
+    df1 = ( matches
+           .groupby('home_team_id', as_index=0)
+           .agg({'away_team_id':'count', 'home_team_goals':'sum', 'away_team_goals':'sum', 'home_team_points':'sum'})
+           .rename(columns={'home_team_id':'team_id', 'away_team_id':'matches_played',
+                            'home_team_goals':'goal_for', 'away_team_goals':'goal_against',
+                            'home_team_points': 'points'})
+          )
+    df2 = ( matches
+           .groupby('away_team_id', as_index=0)
+           .agg({'home_team_id':'count', 'home_team_goals':'sum', 'away_team_goals':'sum', 'away_team_points':'sum'})
+           .rename(columns={'away_team_id':'team_id', 'home_team_id':'matches_played',
+                            'home_team_goals':'goal_against', 'away_team_goals':'goal_for',
+                            'away_team_points': 'points'})
+          )
+    df = ( pd
+          .concat([df1, df2])
+          .groupby('team_id', as_index=0)
+          .agg({'matches_played':'sum', 'goal_for':'sum', 'goal_against':'sum', 'points':'sum'})
+          .merge(teams, how='inner', on='team_id')[['team_name','matches_played','points','goal_for','goal_against']]
+         )
+    df['goal_diff'] = df['goal_for'] - df['goal_against']
+    return df.sort_values(by=['points', 'goal_diff', 'team_name'], ascending=[False, False, True])
 
