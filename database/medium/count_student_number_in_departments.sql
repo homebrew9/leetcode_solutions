da6bdbda5158e8f1e2d50cd317a43bfdc144fe51
Pratyush Joshi <dmodeler@gmail.com>
2025-04-02 23:56:09 -0400

    Added Oracle solution for Database - Count student number in departments

---
+++ database/medium/count_student_number_in_departments.sql
@@ -0,0 +1,25 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+with t (dept_id, dept_name, student_number) as (
+select d.dept_id, d.dept_name, count(student_id) as student_number
+from department d
+     left outer join student s on (s.dept_id = d.dept_id)
+group by d.dept_id, d.dept_name
+)
+select dept_name, student_number
+from t
+order by student_number desc, dept_name
+;
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+
