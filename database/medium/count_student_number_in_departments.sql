daf4b93f5c7da07b8328c15fa4b44396a54d96fe
Pratyush Joshi <dmodeler@gmail.com>
2025-04-03 00:00:12 -0400

    Added SQL Server solution for Database - Count student number in departments

--- database/medium/count_student_number_in_departments.sql
+++ database/medium/count_student_number_in_departments.sql
@@ -29,14 +29,14 @@ select dept_name, student_number
 -- SQL Server
 /* Write your T-SQL query statement below */
 with t (dept_id, dept_name, student_number) as (
-select d.dept_id, d.dept_name, count(student_id) as student_number
-from department d
-     left outer join student s on (s.dept_id = d.dept_id)
-group by d.dept_id, d.dept_name
+    select d.dept_id, d.dept_name, count(student_id) as student_number
+      from department d
+           left outer join student s on (s.dept_id = d.dept_id)
+     group by d.dept_id, d.dept_name
 )
 select dept_name, student_number
-from t
-order by student_number desc, dept_name
+  from t
+ order by student_number desc, dept_name
 ;
 
 
