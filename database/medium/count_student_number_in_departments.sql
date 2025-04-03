49253265774736d352e2a6b397b3e646d72b6158
Pratyush Joshi <dmodeler@gmail.com>
2025-04-02 23:57:41 -0400

    Added MySQL solution for Database - Count student number in departments

--- database/medium/count_student_number_in_departments.sql
+++ database/medium/count_student_number_in_departments.sql
@@ -41,6 +41,17 @@ order by student_number desc, dept_name
 
 
 # MySQL
+# Write your MySQL query statement below
+with t (dept_id, dept_name, student_number) as (
+select d.dept_id, d.dept_name, count(student_id) as student_number
+from department d
+     left outer join student s on (s.dept_id = d.dept_id)
+group by d.dept_id, d.dept_name
+)
+select dept_name, student_number
+from t
+order by student_number desc, dept_name
+;
 
 
 # Pandas
