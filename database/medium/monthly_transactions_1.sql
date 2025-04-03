8a26ab1d9994fb33fb6aff92b66364189e04f492
Pratyush Joshi <dmodeler@gmail.com>
2025-04-02 23:46:18 -0400

    Added SQL Server solution for Database - Monthly transactions I

--- database/medium/monthly_transactions_1.sql
+++ database/medium/monthly_transactions_1.sql
@@ -26,6 +26,15 @@ select to_char(trans_date, 'yyyy-mm') as month,
 
 
 -- SQL Server
+select left(convert(varchar(max), trans_date, 23), 7) as month,
+       country,
+	   count(*) as trans_count,
+	   sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+	   sum(amount) as trans_total_amount,
+	   sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+  from transactions
+ group by left(convert(varchar(max), trans_date, 23), 7), country
+;
 
 
 # MySQL
