9abe22b751e327d44efb9b33cea56d3864a20086
Pratyush Joshi <dmodeler@gmail.com>
2025-04-02 23:46:52 -0400

    Added MySQL Server solution for Database - Monthly transactions I

--- database/medium/monthly_transactions_1.sql
+++ database/medium/monthly_transactions_1.sql
@@ -38,6 +38,16 @@ select left(convert(varchar(max), trans_date, 23), 7) as month,
 
 
 # MySQL
+# Write your MySQL query statement below
+select date_format(trans_date, '%Y-%m') as month,
+       country,
+       count(*) as trans_count,
+       sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+       sum(amount) as trans_total_amount,
+       sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+  from transactions
+ group by date_format(trans_date, '%Y-%m'), country
+;
 
 
 # Pandas
