d344bdfff7bb46385505487bdebedb704f28c445
Pratyush Joshi <dmodeler@gmail.com>
2025-04-02 23:59:38 -0400

    Added PostgreSQL solution for Database - Count student number in departments

--- database/medium/count_student_number_in_departments.sql
+++ database/medium/count_student_number_in_departments.sql
@@ -15,14 +15,14 @@ select dept_name, student_number
 -- PostgreSQL
 -- Write your PostgreSQL query statement below
 with t (dept_id, dept_name, student_number) as (
-select d.dept_id, d.dept_name, count(student_id) as student_number
-from department d
-     left outer join student s on (s.dept_id = d.dept_id)
-group by d.dept_id, d.dept_name
+    select d.dept_id, d.dept_name, count(student_id) as student_number
+      from department d
+           left outer join student s on (s.dept_id = d.dept_id)
+     group by d.dept_id, d.dept_name
 )
 select dept_name, student_number
-from t
-order by student_number desc, dept_name
+  from t
+ order by student_number desc, dept_name
 ;
 
 
