735cad3c16fee378c8344d54446480f6431d2e5c
Pratyush Joshi <dmodeler@gmail.com>
2025-04-05 19:34:23 -0400

    Added SQL Server solution for Database - Analyze subscription conversion

--- database/medium/analyze_subscription_conversion.sql
+++ database/medium/analyze_subscription_conversion.sql
@@ -5,6 +5,20 @@
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t as (
+    select user_id, activity_type, round(avg(convert(float, activity_duration)), 2) as avg_duration
+      from UserActivity
+     group by user_id, activity_type
+)
+select user_id,
+       max(case when activity_type = 'free_trial' then avg_duration end) as trial_avg_duration,
+       max(case when activity_type = 'paid' then avg_duration end) as paid_avg_duration
+  from t
+ group by user_id
+ having max(case when activity_type = 'paid' then avg_duration end) is not null
+ order by user_id
+;
 
 
 # MySQL
