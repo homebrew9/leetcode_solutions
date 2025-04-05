0d7110e74656ff3df78ea390e3957bb7d63b9acf
Pratyush Joshi <dmodeler@gmail.com>
2025-04-05 19:36:21 -0400

    Added PostgreSQL solution for Database - Analyze subscription conversion

--- database/medium/analyze_subscription_conversion.sql
+++ database/medium/analyze_subscription_conversion.sql
@@ -3,6 +3,21 @@
 
 -- PostgreSQL
 -- Write your PostgreSQL query statement below
+-- Solution 1
+-- Write your PostgreSQL query statement below
+with t as (
+    select user_id, activity_type, round(avg(activity_duration), 2) as avg_duration
+      from UserActivity
+     group by user_id, activity_type
+)
+select user_id,
+       max(case when activity_type = 'free_trial' then avg_duration end) as trial_avg_duration,
+       max(case when activity_type = 'paid' then avg_duration end) as paid_avg_duration
+  from t
+ group by user_id
+ having max(case when activity_type = 'paid' then avg_duration end) is not null
+ order by user_id
+;
 
 -- Solution 2
 with t_free as (
