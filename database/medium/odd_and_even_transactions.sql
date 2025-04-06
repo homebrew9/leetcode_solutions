b32cc129be1b58f1ee27668c2e1e4dfcb34d836c
Pratyush Joshi <dmodeler@gmail.com>
2025-04-06 18:36:59 -0400

    Added Oracle solution for Database - Odd and even transactions

--- database/medium/odd_and_even_transactions.sql
+++ database/medium/odd_and_even_transactions.sql
@@ -1,4 +1,12 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+select to_char(transaction_date, 'yyyy-mm-dd') as transaction_date,
+       sum(case when mod(amount, 2) = 1 then amount else 0 end) as odd_sum,
+       sum(case when mod(amount, 2) = 0 then amount else 0 end) as even_sum
+  from transactions
+ group by transaction_date
+ order by transaction_date
+;
 
 
 -- PostgreSQL
