d5fa6c29aeff18b8eab1f1c8abdd425b61fce684
Pratyush Joshi <dmodeler@gmail.com>
2025-04-06 18:35:19 -0400

    Added SQL Server solution for Database - Odd and even transactions

--- database/medium/odd_and_even_transactions.sql
+++ database/medium/odd_and_even_transactions.sql
@@ -5,6 +5,14 @@
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select transaction_date,
+       sum(case when amount % 2 = 1 then amount else 0 end) as odd_sum,
+       sum(case when amount % 2 = 0 then amount else 0 end) as even_sum
+  from transactions
+ group by transaction_date
+ order by transaction_date
+;
 
 
 # MySQL
