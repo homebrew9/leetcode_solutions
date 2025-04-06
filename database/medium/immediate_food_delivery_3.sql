f2e78251a2871cd0149ea88085ee2ebdf395927a
Pratyush Joshi <dmodeler@gmail.com>
2025-04-06 18:32:04 -0400

    Added Oracle solution for Database - Immediate food delivery III

--- database/medium/immediate_food_delivery_3.sql
+++ database/medium/immediate_food_delivery_3.sql
@@ -1,4 +1,18 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+select to_char(order_date, 'YYYY-MM-DD') as order_date,
+       round(
+              sum(case order_date when customer_pref_delivery_date then 1 else 0 end)
+              /
+              count(*)
+              *
+              100,
+              2
+            ) as immediate_percentage
+from delivery
+group by order_date
+order by order_date
+;
 
 
 -- PostgreSQL
