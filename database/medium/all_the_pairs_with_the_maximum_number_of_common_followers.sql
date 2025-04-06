8f9f260076e11c734d04207c6ea11ce965f267b3
Pratyush Joshi <dmodeler@gmail.com>
2025-04-06 18:10:07 -0400

    Added MySQL solution for Database - All the pairs with the maximum number of common followers

--- database/medium/all_the_pairs_with_the_maximum_number_of_common_followers.sql
+++ database/medium/all_the_pairs_with_the_maximum_number_of_common_followers.sql
@@ -8,6 +8,18 @@
 
 
 # MySQL
+# Write your MySQL query statement below
+with t (user1_id, user2_id, follower_count) as (
+select x.user_id, y.user_id, count(*) as followers_count
+from relations x
+     inner join relations y on (x.user_id < y.user_id and x.follower_id = y.follower_id)
+group by x.user_id, y.user_id
+#order by x.user_id, y.user_id
+)
+select user1_id, user2_id
+from t
+where follower_count = (select max(follower_count) from t)
+;
 
 
 # Pandas
