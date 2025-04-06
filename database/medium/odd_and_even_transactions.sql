a1efd2deeee4d5695575d0c130939f67f4c97795
Pratyush Joshi <dmodeler@gmail.com>
2025-04-06 18:34:49 -0400

    Added MySQL solution for Database - Odd and even transactions

--- database/medium/odd_and_even_transactions.sql
+++ database/medium/odd_and_even_transactions.sql
@@ -8,6 +8,14 @@
 
 
 # MySQL
+# Write your MySQL query statement below
+select transaction_date,
+       sum(case when mod(amount, 2) = 1 then amount else 0 end) as odd_sum,
+       sum(case when mod(amount, 2) = 0 then amount else 0 end) as even_sum
+  from transactions
+ group by transaction_date
+ order by transaction_date
+;
 
 
 # Pandas
