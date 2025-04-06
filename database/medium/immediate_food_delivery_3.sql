b923a07d836140e16c9c9eed2f589378d8df959c
Pratyush Joshi <dmodeler@gmail.com>
2025-04-06 18:30:17 -0400

    Added MySQL solution for Database - Immediate food delivery III

---
+++ database/medium/immediate_food_delivery_3.sql
@@ -0,0 +1,21 @@
+-- Oracle
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+# Write your MySQL query statement below
+select order_date,
+       round(sum(case order_date when customer_pref_delivery_date then 1 else 0 end)/count(*)*100, 2) as immediate_percentage
+from delivery
+group by order_date
+order by order_date
+;
+
+
+# Pandas
+
