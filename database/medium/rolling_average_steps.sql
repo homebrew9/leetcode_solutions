482fb70ebb6f5e6bfa733656c1cab3d95a34d4a8
Pratyush Joshi <dmodeler@gmail.com>
2025-04-07 23:04:48 -0400

    Added MySQL solution for Database - Rolling average steps

---
+++ database/medium/rolling_average_steps.sql
@@ -0,0 +1,23 @@
+-- Oracle
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+# Write your MySQL query statement below
+-- Old-style
+select s1.user_id, s1.steps_date,
+       round((s1.steps_count+s2.steps_count+s3.steps_count)/3, 2) as rolling_average
+from steps s1
+     inner join steps s2 on (s2.user_id = s1.user_id and s2.steps_date = DATE_SUB(s1.steps_date, INTERVAL 1 DAY))
+     inner join steps s3 on (s3.user_id = s2.user_id and s3.steps_date = DATE_SUB(s2.steps_date, INTERVAL 1 DAY))
+order by s1.user_id, s1.steps_date
+;
+
+
+# Pandas
+
