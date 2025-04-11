472de639dbf5b9bfbdd6742da187f495f49fafc9
Pratyush Joshi <dmodeler@gmail.com>
2025-04-10 22:19:00 -0400

    Added PostgreSQL solution for Database - Binary tree nodes

--- database/medium/binary_tree_nodes.sql
+++ database/medium/binary_tree_nodes.sql
@@ -2,6 +2,24 @@
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+with recursive t_hier(node, parent, type) as (
+    select t.n, t.p, 'Root'
+      from tree t
+     where t.p is null
+    union all
+    select t1.n, t1.p,
+           case when (select count(*) from tree where p = t1.n) = 0
+                then 'Leaf'
+                else 'Inner'
+           end
+      from tree t1
+           inner join t_hier t on (t.node = t1.p)
+)
+select node as n, type
+from t_hier
+order by node
+;
 
 
 -- SQL Server
