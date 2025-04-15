0dbac0f7638c7c1fe42421419e7b8f52b850b563
Pratyush Joshi <dmodeler@gmail.com>
2025-04-14 23:04:24 -0400

    Added SQL Server solution for Database - Project employees II

--- database/easy/project_employees_2.sql
+++ database/easy/project_employees_2.sql
@@ -5,6 +5,21 @@
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t (project_id, employee_count) as (
+    select project_id, count(employee_id)
+      from project
+     group by project_id
+),
+t1 (project_id, employee_count, max_employee_count) as (
+    select project_id, employee_count,
+           max(employee_count) over () as max_employee_count
+      from t
+)
+select project_id as "project_id"
+from t1
+where employee_count = max_employee_count
+;
 
 
 # MySQL
