18da938b9e307735092bb71086f769b2966238eb
Pratyush Joshi <dmodeler@gmail.com>
2025-04-14 22:59:23 -0400

    Added PostgreSQL solution for Database - Rising temperature

--- database/easy/rising_temperature.sql
+++ database/easy/rising_temperature.sql
@@ -2,6 +2,20 @@
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t as (
+    select id, recordDate, temperature,
+           case
+                when recordDate = lag(recordDate) over (order by recordDate) + 1 and
+                     temperature > lag(temperature) over (order by recordDate)
+                then 'Y'
+           end as is_higher
+      from weather
+)
+select id
+  from t
+ where is_higher = 'Y'
+;
 
 
 -- SQL Server
