dae60006e00324b798bb30bd980fe6d6050e1ff9
Pratyush Joshi <dmodeler@gmail.com>
2025-04-15 23:49:20 -0400

    Added Oracle solution for Database - The users that are eligible for discount

--- database/easy/the_users_that_are_eligible_for_discount.sql
+++ database/easy/the_users_that_are_eligible_for_discount.sql
@@ -1,4 +1,16 @@
 -- Oracle
+CREATE FUNCTION getUserIDs(startDate IN DATE, endDate IN DATE, minAmount IN NUMBER)
+RETURN SYS_REFCURSOR IS result SYS_REFCURSOR;
+BEGIN
+    /* Write your PL/SQL query statement below */
+   open result for
+   select distinct user_id
+      from purchases
+     where time_stamp between startDate and endDate
+       and amount >= minAmount
+     order by user_id;
+    RETURN result;
+END;
 
 
 -- PostgreSQL
