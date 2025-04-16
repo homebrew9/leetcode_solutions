e118965ad9647156866df44cc4103d4f5d2ff72e
Pratyush Joshi <dmodeler@gmail.com>
2025-04-15 23:48:40 -0400

    Added PostgreSQL solution for Database - The users that are eligible for discount

--- database/easy/the_users_that_are_eligible_for_discount.sql
+++ database/easy/the_users_that_are_eligible_for_discount.sql
@@ -2,6 +2,19 @@
 
 
 -- PostgreSQL
+CREATE OR REPLACE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT)
+RETURNS TABLE (user_id INT) AS $$
+BEGIN
+  RETURN QUERY (
+      -- Write your PostgreSQL query statement below.
+   select distinct p.user_id
+      from purchases p
+     where p.time_stamp between startDate and endDate
+       and p.amount >= minAmount
+     order by p.user_id
+  );
+END;
+$$ LANGUAGE plpgsql;
 
 
 -- SQL Server
