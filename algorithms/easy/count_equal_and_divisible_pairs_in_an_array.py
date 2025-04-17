3729b492edb7542f70e04ae0464dd221c52ef002
Pratyush Joshi <dmodeler@gmail.com>
2025-04-16 23:22:28 -0400

    Updated the solution for Count equal and divisible pairs in an array

--- algorithms/easy/count_equal_and_divisible_pairs_in_an_array.py
+++ algorithms/easy/count_equal_and_divisible_pairs_in_an_array.py
@@ -1,13 +1,27 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
+        # TC = O(N^2), SC = O(1)
         N = len(nums)
         res = 0
         for i in range(N):
             for j in range(i+1, N):
                 if nums[i] == nums[j] and (i * j) % k == 0:
                     res += 1
+        return res
+    def countPairs_1(self, nums: List[int], k: int) -> int:
+        # TC = O(N), SC = O(N)
+        N = len(nums)
+        hsh = defaultdict(list)
+        res = 0
+        for i, v in enumerate(nums):
+            if v in hsh:
+                for j in hsh[v]:
+                    if (i * j) % k == 0:
+                        res += 1
+            hsh[v] += [i]
         return res
 
 # Main section
@@ -18,6 +32,8 @@ for nums, k in [
     print(f'nums, k = {nums}, {k}')
     sol = Solution()
     r = sol.countPairs(nums, k)
-    print(f'r = {r}')
+    r1 = sol.countPairs_1(nums, k)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('=============')
 
