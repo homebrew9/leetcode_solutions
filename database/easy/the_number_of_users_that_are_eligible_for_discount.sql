6d2438d44317661742cf696c16845f3171b3862f
Pratyush Joshi <dmodeler@gmail.com>
2025-04-16 23:33:19 -0400

    Added SQL Server solution for Database - The number of users that are eligible for discount

--- database/easy/the_number_of_users_that_are_eligible_for_discount.sql
+++ database/easy/the_number_of_users_that_are_eligible_for_discount.sql
@@ -5,6 +5,16 @@
 
 
 -- SQL Server
+CREATE FUNCTION getUserIDs(@startDate DATE, @endDate DATE, @minAmount INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        /* Write your T-SQL query statement below. */
+      select count(distinct user_id)
+        from purchases
+       where time_stamp between @startDate and @endDate
+         and amount >= @minAmount
+    );
+END
 
 
 # MySQL
