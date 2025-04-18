25292e6411b1d54fbe07bb72356f600c8351cbf7
Pratyush Joshi <dmodeler@gmail.com>
2025-04-18 19:42:10 -0400

    Added SQL Server solution for Database - Top travellers

--- database/easy/top_travellers.sql
+++ database/easy/top_travellers.sql
@@ -27,6 +27,17 @@ select t.name, t.travelled_distance
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t as (
+    select u.id, u.name, coalesce(sum(r.distance), 0) as travelled_distance
+      from users u
+           left join rides r on (r.user_id = u.id)
+     group by u.id, u.name
+)
+select t.name, t.travelled_distance
+  from t
+ order by t.travelled_distance desc, t.name
+;
 
 
 # MySQL
