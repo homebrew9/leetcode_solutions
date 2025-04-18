e272d2fb05df140648f3e1ee8a1c3164a81a9afe
Pratyush Joshi <dmodeler@gmail.com>
2025-04-18 19:37:41 -0400

    Added SQL Server solution for Database - The number of employees which report to each employee

--- database/easy/the_number_of_employees_which_report_to_each_employee.sql
+++ database/easy/the_number_of_employees_which_report_to_each_employee.sql
@@ -35,6 +35,16 @@ select e1.reports_to as employee_id,
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select e1.reports_to as employee_id,
+       e2.name,
+       count(e1.employee_id) as reports_count,
+       round(avg(convert(float, e1.age)), 0) as average_age
+  from employees e1
+       inner join employees e2 on (e2.employee_id = e1.reports_to)
+ group by e1.reports_to, e2.name
+ order by e1.reports_to
+;
 
 
 # MySQL
