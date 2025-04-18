9fecc1f81ab340d6e0fa930701cd43db8b73cae5
Pratyush Joshi <dmodeler@gmail.com>
2025-04-18 19:46:11 -0400

    Added MySQL solution for Database - Calculate special bonus

--- database/easy/calculate_special_bonus.sql
+++ database/easy/calculate_special_bonus.sql
@@ -14,6 +14,15 @@ select employee_id,
 
 
 # MySQL
+# Write your MySQL query statement below
+select employee_id,
+       case when mod(employee_id, 2) = 1 and substring(name,1,1) != 'M'
+            then salary
+            else 0
+       end as bonus
+from employees
+order by employee_id
+;
 
 
 # Pandas
