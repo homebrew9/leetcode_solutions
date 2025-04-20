30dc36c8fef9e4f312b8e7a414fa73f3ab815160
Pratyush Joshi <dmodeler@gmail.com>
2025-04-19 23:06:23 -0400

    Added solution for Database - Calculate special bonus

--- database/easy/calculate_special_bonus.sql
+++ database/easy/calculate_special_bonus.sql
@@ -8,9 +8,19 @@ select employee_id,
 
 
 -- PostgreSQL
+select employee_id,
+       case when mod(employee_id, 2) = 1 and name not like 'M%' then salary else 0 end as bonus
+  from employees
+ order by employee_id
+;
 
 
 -- SQL Server
+select employee_id,
+       case when employee_id % 2 = 1 and name not like 'M%' then salary else 0 end as bonus
+  from employees
+ order by employee_id
+;
 
 
 # MySQL
@@ -26,4 +36,13 @@ order by employee_id
 
 
 # Pandas
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    # np.where is similar to Oracle's DECODE function
+    employees['bonus'] = np.where(
+                             (
+                                  employees['employee_id']%2 == 1) & (~employees['name'].str.startswith('M')
+                             ), employees['salary'], 0)
+    return employees[['employee_id','bonus']].sort_values('employee_id')
 
