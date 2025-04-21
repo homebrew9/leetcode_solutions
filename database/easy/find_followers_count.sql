3563f0048db6264ff3e7ebee9ee5909f773e4d3a
Pratyush Joshi <dmodeler@gmail.com>
2025-04-21 18:45:57 -0400

    Added solution for Database - Find followers count

--- database/easy/find_followers_count.sql
+++ database/easy/find_followers_count.sql
@@ -8,9 +8,21 @@ order by user_id
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+select user_id, count(*) as followers_count
+  from followers
+ group by user_id
+ order by user_id
+;
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select user_id, count(*) as followers_count
+  from followers
+ group by user_id
+ order by user_id
+;
 
 
 # MySQL
@@ -23,4 +35,25 @@ order by user_id
 
 
 # Pandas
+# Solution 1
+import pandas as pd
+
+def count_followers(followers: pd.DataFrame) -> pd.DataFrame:
+    return ( followers
+            .groupby('user_id', as_index=0)
+            .agg(followers_count=('follower_id','count'))
+            .sort_values('user_id')
+           )
+
+# Solution 2
+import pandas as pd
+
+def count_followers(followers: pd.DataFrame) -> pd.DataFrame:
+    return ( followers
+            .groupby('user_id', as_index=0)
+            .size()
+            .rename(columns={'size':'followers_count'})
+            .sort_values('user_id')
+           )
+
 
