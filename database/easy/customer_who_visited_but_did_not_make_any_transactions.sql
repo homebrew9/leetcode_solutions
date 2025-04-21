7077d984c363a580c6810c10e8788db0ebc860cd
Pratyush Joshi <dmodeler@gmail.com>
2025-04-21 19:37:38 -0400

    Added solution for Database - Customer who visited but did not make any transactions

--- database/easy/customer_who_visited_but_did_not_make_any_transactions.sql
+++ database/easy/customer_who_visited_but_did_not_make_any_transactions.sql
@@ -8,9 +8,29 @@ group by v.customer_id
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+select v.customer_id,
+       count(v.visit_id) as count_no_trans
+  from visits v
+ where not exists ( select null
+                      from transactions t
+                     where t.visit_id = v.visit_id
+                  )
+ group by v.customer_id
+;
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select v.customer_id,
+       count(v.visit_id) as count_no_trans
+  from visits v
+ where not exists ( select null
+                      from transactions t
+                     where t.visit_id = v.visit_id
+                  )
+ group by v.customer_id
+;
 
 
 # MySQL
@@ -23,4 +43,11 @@ group by v.customer_id
 
 
 # Pandas
+import pandas as pd
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    return ( visits[~visits['visit_id'].isin(transactions['visit_id'])]
+            .groupby('customer_id', as_index=0)
+            .agg(count_no_trans=('visit_id', 'count'))
+           )
 
