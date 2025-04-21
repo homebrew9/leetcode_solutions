62c600c6ba3abc78303ac39e3c722b7f05bc742c
Pratyush Joshi <dmodeler@gmail.com>
2025-04-21 19:29:37 -0400

    Added solution for Database - Big countries

--- database/easy/big_countries.sql
+++ database/easy/big_countries.sql
@@ -7,9 +7,21 @@ where area >= 3000000 or population >= 25000000
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+select name, population, area
+  from world
+ where area >= 3000000
+    or population >= 25000000
+;
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+select name, population, area
+  from world
+ where area >= 3000000
+    or population >= 25000000
+;
 
 
 # MySQL
