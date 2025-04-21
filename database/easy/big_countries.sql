27e0d818a7386553b6060dee5a4702eaf845cd6f
Pratyush Joshi <dmodeler@gmail.com>
2025-04-21 18:29:48 -0400

    Added solution for Database - Big countries

---
+++ database/easy/big_countries.sql
@@ -0,0 +1,29 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+select name, population, area
+from world
+where area >= 3000000 or population >= 25000000
+;
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+# Write your MySQL query statement below
+select name, population, area
+  from world
+where area >= 3000000
+   or population >= 25000000
+;
+
+
+# Pandas
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    return world[(world['area'] >= 3_000_000) | (world['population'] >= 25_000_000)][['name','population','area']]
+
