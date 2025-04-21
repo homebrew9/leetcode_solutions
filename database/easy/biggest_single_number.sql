2a662cfab375baeb62d89d7c45f91577316d0284
Pratyush Joshi <dmodeler@gmail.com>
2025-04-21 18:59:06 -0400

    Added solution for Database - Biggest single number

--- database/easy/biggest_single_number.sql
+++ database/easy/biggest_single_number.sql
@@ -11,13 +11,56 @@ where num_occurrences = 1
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t (num, frequency) as (
+    select num, count(*) as frequency
+      from mynumbers
+     group by num
+)
+select max(num) as num
+  from t
+ where frequency = 1
+;
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t (num, frequency) as (
+    select num, count(*) as frequency
+      from mynumbers
+     group by num
+)
+select max(num) as num
+  from t
+ where frequency = 1
+;
 
 
 # MySQL
+# Write your MySQL query statement below
+with t (num, frequency) as (
+    select num, count(*) as frequency
+      from mynumbers
+     group by num
+)
+select max(num) as num
+  from t
+ where frequency = 1
+;
 
 
 # Pandas
+import pandas as pd
+
+def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame({
+               'num': [
+                         my_numbers['num']
+                        .value_counts()
+                        .to_frame()
+                        .query('count==1')
+                        .index
+                        .max()
+                      ]
+            })
 
