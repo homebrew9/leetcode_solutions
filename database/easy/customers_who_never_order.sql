14ac4f7d320e61ade3f1907e324368d0e8dabe93
Pratyush Joshi <dmodeler@gmail.com>
2025-04-22 23:38:04 -0400

    Added Pandas solution for Database - Customers who never order

--- database/easy/customers_who_never_order.sql
+++ database/easy/customers_who_never_order.sql
@@ -24,4 +24,8 @@ where c.id not in (select o.customerid from orders o)
 
 
 # Pandas
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    return customers[~customers['id'].isin(orders.customerId)][['name']].rename(columns={'name': 'Customers'})
 
