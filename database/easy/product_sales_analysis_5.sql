f27fa5bb6ab9d2393ca9f2b865e48e6b3d76e099
Pratyush Joshi <dmodeler@gmail.com>
2025-04-22 23:32:34 -0400

    Added MySQL solution for Database - Product sales analysis V

--- database/easy/product_sales_analysis_5.sql
+++ database/easy/product_sales_analysis_5.sql
@@ -8,6 +8,13 @@
 
 
 # MySQL
+# Write your MySQL query statement below
+select s.user_id, sum(s.quantity*p.price) as spending
+from sales s
+     inner join product p on (p.product_id = s.product_id)
+group by s.user_id
+order by spending desc, user_id
+;
 
 
 # Pandas
