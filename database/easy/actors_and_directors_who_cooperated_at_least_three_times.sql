5dd33bfe6176e1fba38c29b8a02bee95a146ca70
Pratyush Joshi <dmodeler@gmail.com>
2025-04-23 23:37:16 -0400

    Added Oracle solution for Database - Actors and directors who cooperated at least three times

--- database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
+++ database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -1,5 +1,10 @@
 -- Oracle
-
+/* Write your PL/SQL query statement below */
+select actor_id, director_id
+  from ActorDirector
+ group by actor_id, director_id
+having count(*) >= 3
+;
 
 -- PostgreSQL
 -- Write your PostgreSQL query statement below
