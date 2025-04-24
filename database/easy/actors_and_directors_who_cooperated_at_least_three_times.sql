d1e6d8a1d97a335fdccef96848cdab08c46842b9
Pratyush Joshi <dmodeler@gmail.com>
2025-04-23 23:38:50 -0400

    Added Pandas solution for Database - Actors and directors who cooperated at least three times

--- database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
+++ database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -29,4 +29,13 @@ having count(*) >= 3
 
 
 # Pandas
+import pandas as pd
 
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    return actor_director.groupby(
+               by=['actor_id','director_id'], as_index=False
+           ).agg(
+               cooperated=('actor_id','count')
+           ).query(
+               'cooperated >= 3'
+           )[['actor_id','director_id']]
