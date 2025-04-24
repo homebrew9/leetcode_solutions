c280797531ce31453078cf8fcd733eeed3059232
Pratyush Joshi <dmodeler@gmail.com>
2025-04-23 23:44:54 -0400

    Added Oracle solution for Database - Employees earning more than their managers

--- database/easy/employees_earning_more_than_their_managers.sql
+++ database/easy/employees_earning_more_than_their_managers.sql
@@ -1,4 +1,10 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+select e.name as "Employee"
+  from employee e
+       inner join employee m
+       on (e.managerid = m.id and e.salary > m.salary)
+;
 
 
 -- PostgreSQL
