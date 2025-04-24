f44e7ebb086f1d5be247886c7a1010fe05b7ec9f
Pratyush Joshi <dmodeler@gmail.com>
2025-04-23 23:37:54 -0400

    Added MySQL solution for Database - Actors and directors who cooperated at least three times

--- database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
+++ database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -20,7 +20,12 @@ having count(*) >= 3
 
 
 # MySQL
-
+# Write your MySQL query statement below
+select actor_id, director_id
+from actordirector
+group by actor_id, director_id
+having count(*) >= 3
+;
 
 # Pandas
 
