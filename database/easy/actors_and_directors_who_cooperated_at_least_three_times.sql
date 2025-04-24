a3ac7e9a1b45f3d7636a622fe1a23b347458c401
Pratyush Joshi <dmodeler@gmail.com>
2025-04-23 23:36:26 -0400

    Added PostgreSQL solution for Database - Actors and directors who cooperated at least three times

---
+++ database/easy/actors_and_directors_who_cooperated_at_least_three_times.sql
@@ -0,0 +1,19 @@
+-- Oracle
+
+
+-- PostgreSQL
+-- Write your PostgreSQL query statement below
+select actor_id, director_id
+  from ActorDirector
+ group by actor_id, director_id
+having count(*) >= 3
+;
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+
