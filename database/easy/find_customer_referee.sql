95b223b35f42e6889174ae20f77c27515f0d8d49
Pratyush Joshi <dmodeler@gmail.com>
2025-04-24 23:32:34 -0400

    Added Pandas solution for Database - Find customer referee

---
+++ database/easy/find_customer_referee.sql
@@ -0,0 +1,18 @@
+-- Oracle
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    return customer[(customer['referee_id'].isna()) | (customer['referee_id'] != 2)][['name']]
+
