b02739e19058497baadc9206b6851b60c0b69c17
Pratyush Joshi <dmodeler@gmail.com>
2025-04-24 23:23:59 -0400

    Added SQL Server solution for Database - Primary department for each employee

--- database/easy/primary_department_for_each_employee.sql
+++ database/easy/primary_department_for_each_employee.sql
@@ -16,6 +16,17 @@ select employee_id, department_id
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t (employee_id, department_id, primary_flag, dept_count) as (
+    select employee_id, department_id, primary_flag,
+           count(*) over (partition by employee_id) as dept_count
+      from employee
+)
+select employee_id, department_id
+  from t
+ where dept_count = 1
+    or primary_flag = 'Y'
+;
 
 
 # MySQL
