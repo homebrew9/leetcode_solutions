1164deb89ae0ca4c1d646edbfec7d086ce4ba0f8
Pratyush Joshi <dmodeler@gmail.com>
2025-04-24 23:23:11 -0400

    Added MySQL solution for Database - Primary department for each employee

--- database/easy/primary_department_for_each_employee.sql
+++ database/easy/primary_department_for_each_employee.sql
@@ -19,6 +19,17 @@ select employee_id, department_id
 
 
 # MySQL
+# Write your MySQL query statement below
+with t (employee_id, department_id, primary_flag, dept_count) as (
+    select employee_id, department_id, primary_flag,
+           count(*) over (partition by employee_id) as dept_count
+      from employee
+)
+select employee_id, department_id
+  from t
+ where dept_count = 1
+    or primary_flag = 'Y'
+;
 
 
 # Pandas
