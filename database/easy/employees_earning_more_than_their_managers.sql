9f8f182212f9411d0564fd7ccabc4659476f5937
Pratyush Joshi <dmodeler@gmail.com>
2025-04-24 23:29:12 -0400

    Added Pandas solution for Database - Employees earning more than their managers

--- database/easy/employees_earning_more_than_their_managers.sql
+++ database/easy/employees_earning_more_than_their_managers.sql
@@ -35,4 +35,12 @@ select e.name as Employee
 
 
 # Pandas
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    return ( employee
+            .merge(employee, how='inner', left_on='managerId', right_on='id')
+            .query('salary_x > salary_y')[['name_x']]
+            .rename(columns={'name_x': 'Employee'})
+           )
 
