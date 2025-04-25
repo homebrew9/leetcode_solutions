62d533f7956d8aa7576b5e3528d577fd75779dc0
Pratyush Joshi <dmodeler@gmail.com>
2025-04-24 23:25:21 -0400

    Added Oracle solution for Database - Primary department for each employee

--- database/easy/primary_department_for_each_employee.sql
+++ database/easy/primary_department_for_each_employee.sql
@@ -1,4 +1,14 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+with t (employee_id, department_id, primary_flag, dept_count) as (
+select employee_id, department_id, primary_flag,
+       count(*) over (partition by employee_id) as dept_count
+from employee
+)
+select employee_id, department_id
+from t
+where dept_count = 1 or (dept_count > 1 and primary_flag = 'Y')
+;
 
 
 -- PostgreSQL
