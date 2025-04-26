58e04329f64fa66e9fd7aa20262ec8752ce129b6
Pratyush Joshi <dmodeler@gmail.com>
2025-04-25 23:46:14 -0400

    Added Pandas solution for Database - List the products ordered in a period

---
+++ database/easy/list_the_products_ordered_in_a_period.sql
@@ -0,0 +1,25 @@
+-- Oracle
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+import pandas as pd
+
+def list_products(products: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    return ( orders[orders['order_date'].dt.strftime('%Y-%m') == '2020-02']
+            .groupby('product_id', as_index=0)['unit']
+            .sum()
+            .query('unit >= 100')
+            .merge(products, how='inner', on='product_id')[['product_name', 'unit']]
+           )
+
+
+
