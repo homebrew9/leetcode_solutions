9dde12b5fd63dc73ea3cde711d285765e394aa87
Pratyush Joshi <dmodeler@gmail.com>
2025-04-28 23:34:55 -0400

    Added Pandas solution for Database - Bikes last time used

---
+++ database/easy/bikes_last_time_used.sql
@@ -0,0 +1,21 @@
+-- Oracle
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+import pandas as pd
+
+def last_used_time(bikes: pd.DataFrame) -> pd.DataFrame:
+    return ( bikes
+            .groupby('bike_number', as_index=False)['end_time']
+            .max()
+            .sort_values(by='end_time', ascending=False)
+           )
