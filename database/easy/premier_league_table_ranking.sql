d14d3259af041027a2239d413565135e950629bd
Pratyush Joshi <dmodeler@gmail.com>
2025-04-28 23:43:57 -0400

    Added Oracle solution for Database - Premier league table ranking

--- database/easy/premier_league_table_ranking.sql
+++ database/easy/premier_league_table_ranking.sql
@@ -1,4 +1,10 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+select team_id, team_name, 3 * wins + draws as points,
+       rank() over (order by 3 * wins + draws desc) as position
+  from teamstats
+ order by points desc, team_name
+;
 
 
 -- PostgreSQL
