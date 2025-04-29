d43949a9fadd3bf5c93375951f6cb3504e6d3154
Pratyush Joshi <dmodeler@gmail.com>
2025-04-28 23:38:45 -0400

    Added Oracle solution for Database - Bikes last time used

--- database/easy/bikes_last_time_used.sql
+++ database/easy/bikes_last_time_used.sql
@@ -1,5 +1,10 @@
 -- Oracle
-
+/* Write your PL/SQL query statement below */
+select bike_number, max(end_time) as end_time
+from bikes
+group by bike_number
+order by end_time desc
+;
 
 -- PostgreSQL
 -- Write your PostgreSQL query statement below
