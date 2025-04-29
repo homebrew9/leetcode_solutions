3702050cb8d9f9b370bd7a0cfa57b72306941476
Pratyush Joshi <dmodeler@gmail.com>
2025-04-28 23:37:47 -0400

    Added MySQL solution for Database - Bikes last time used

--- database/easy/bikes_last_time_used.sql
+++ database/easy/bikes_last_time_used.sql
@@ -20,7 +20,12 @@ order by end_time desc
 
 
 # MySQL
-
+# Write your MySQL query statement below
+select bike_number, max(end_time) as end_time
+from bikes
+group by bike_number
+order by end_time desc
+;
 
 # Pandas
 import pandas as pd
