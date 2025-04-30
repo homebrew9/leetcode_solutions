77dba4f2c88a7fe0f787746ec5b78b8993330128
Pratyush Joshi <dmodeler@gmail.com>
2025-04-29 23:46:28 -0400

    Added Oracle solution for Database - Product sales analysis II

--- database/easy/product_sales_analysis_2.sql
+++ database/easy/product_sales_analysis_2.sql
@@ -1,5 +1,10 @@
 -- Oracle
-
+/* Write your PL/SQL query statement below */
+select p.product_id, sum(s.quantity) as total_quantity
+  from product p
+       inner join sales s on (s.product_id = p.product_id)
+ group by p.product_id
+;
 
 -- PostgreSQL
 
