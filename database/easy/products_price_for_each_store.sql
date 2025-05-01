bdbb975cbe67e40b29f55ac863febf1a3fc9cc39
Pratyush Joshi <dmodeler@gmail.com>
2025-04-30 23:36:52 -0400

    Added MySQL solution for Database - Product's price for each store

--- database/easy/products_price_for_each_store.sql
+++ database/easy/products_price_for_each_store.sql
@@ -8,6 +8,20 @@
 
 
 # MySQL
+# Write your MySQL query statement below
+-- Looks like pivoting is complex in MySQL. See links:
+-- https://stackoverflow.com/questions/67021424/using-pivot-to-join-tables-using-mysql-8-version
+-- https://mariadb.com/kb/en/pivoting-in-mariadb/
+-- https://mysql.rjweb.org/doc.php/pivot
+-- We use our trusty old "max decode" trick.
+
+select product_id,
+       max(case when store = 'store1' then price end) as store1,
+       max(case when store = 'store2' then price end) as store2,
+       max(case when store = 'store3' then price end) as store3
+  from products
+ group by product_id
+;
 
 
 # Pandas
