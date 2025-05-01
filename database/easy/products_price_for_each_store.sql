9a43650f530f97af19238141b27f6991ad358211
Pratyush Joshi <dmodeler@gmail.com>
2025-04-30 23:39:25 -0400

    Added Oracle solution for Database - Product's price for each store

--- database/easy/products_price_for_each_store.sql
+++ database/easy/products_price_for_each_store.sql
@@ -1,4 +1,11 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+select *
+from products
+pivot (max(price)
+       for (store) in ('store1' as store1, 'store2' as store2, 'store3' as store3)
+      )
+;
 
 
 -- PostgreSQL
