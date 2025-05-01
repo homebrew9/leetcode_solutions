ec78135e0aa1e7106ea3df33392d89a423190e61
Pratyush Joshi <dmodeler@gmail.com>
2025-04-30 23:28:58 -0400

    Added Oracle solution for Database - Unique orders and customers per month

--- database/easy/unique_orders_and_customers_per_month.sql
+++ database/easy/unique_orders_and_customers_per_month.sql
@@ -1,4 +1,12 @@
 -- Oracle
+/* Write your PL/SQL query statement below */
+select to_char(order_date, 'yyyy-mm') as month,
+       count(distinct order_id) as order_count,
+       count(distinct customer_id) as customer_count
+from orders
+where invoice > 20
+group by to_char(order_date, 'yyyy-mm')
+;
 
 
 -- PostgreSQL
