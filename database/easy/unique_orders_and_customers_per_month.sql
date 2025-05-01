b302573b6282796e65cf54f5991eacc11d6d8479
Pratyush Joshi <dmodeler@gmail.com>
2025-04-30 23:27:40 -0400

    Added MySQL solution for Database - Unique orders and customers per month

--- database/easy/unique_orders_and_customers_per_month.sql
+++ database/easy/unique_orders_and_customers_per_month.sql
@@ -8,6 +8,14 @@
 
 
 # MySQL
+# Write your MySQL query statement below
+select date_format(order_date, '%Y-%m') as month,
+       count(distinct order_id) as order_count,
+       count(distinct customer_id) as customer_count
+from orders
+where invoice > 20
+group by date_format(order_date, '%Y-%m')
+;
 
 
 # Pandas
