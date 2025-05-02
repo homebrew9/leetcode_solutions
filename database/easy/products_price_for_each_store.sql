b5df8994d8900922ff67a085e0ae0f8f250a941d
Pratyush Joshi <dmodeler@gmail.com>
2025-05-01 23:48:25 -0400

    Added PostgreSQL solution for Database - Product's price for each store

--- database/easy/products_price_for_each_store.sql
+++ database/easy/products_price_for_each_store.sql
@@ -20,7 +20,6 @@ select product_id,
   from products
  group by product_id
 ;
-
 -- The LC setup does not have the tablefunc module installed, hence the crosstab queries below fail in LC.
 -- But they can be tested in other PostgreSQL installations that do have the tablefunc module.
 -- If the tablefunc module has been installed, then we can use crosstab function.
@@ -30,9 +29,33 @@ select product_id,
 --
 select *
   from crosstab (
-      'select product_id, store_cd, amount from t_store order by product_id',
-	  'select distinct store_cd from t_store order by store_cd'
-  ) as ct(product_id int, "store_1" int, "store_2" int, "store_3" int, "store_4" int)
+      'select product_id, store, amount from products order by product_id',
+	  'select distinct store from products order by store'
+  ) as ct(product_id int, "store1" int, "store2" int, "store3" int, "store4" int)
+;
+-- The single query version of crosstab works as well - but we have to return all combinations
+-- of (product_id, store) otherwise the amounts are displayed under incorrect stores.
+--
+select *
+  from crosstab
+  (
+        'with t as (
+            select x.product_id, y.store
+              from (select distinct product_id from products) x
+                    cross join
+                   (select distinct store from products) y
+        ),
+        t1 as (
+            select product_id, store, sum(amount) as amount
+              from products
+             group by product_id, store
+        )
+        select t.product_id, t.store, t1.amount::float8
+          from t
+               left outer join t1 on (t1.product_id = t.product_id and t1.store = t.store)
+         order by t.product_id, t.store'
+  )
+  as ct(product_id int, "store1" float8, "store2" float8, "store3" float8, "store4" float8)
 ;
 
 
