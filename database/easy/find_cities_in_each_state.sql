31f68d5e2cf8724346e17ca3353c85fc48144cb7
Pratyush Joshi <dmodeler@gmail.com>
2025-05-01 23:35:14 -0400

    Added SQL Server solution for Database - Find cities in each state

---
+++ database/easy/find_cities_in_each_state.sql
@@ -0,0 +1,19 @@
+-- Oracle
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+/* Write your T-SQL query statement below */
+select state, string_agg(city, ', ') within group (order by city) as cities
+from cities
+group by state
+order by state
+;
+
+# MySQL
+
+
+# Pandas
+
