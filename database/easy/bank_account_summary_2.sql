d0809d54e7b64ae0689c500166c3444182d55c36
Pratyush Joshi <dmodeler@gmail.com>
2025-05-02 23:53:57 -0400

    Added SQL Server solution for Database - Bank account summary II

--- database/easy/bank_account_summary_2.sql
+++ database/easy/bank_account_summary_2.sql
@@ -12,6 +12,17 @@ having sum(t.amount) > 10000
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t as (
+    select account, sum(amount) as balance
+    from transactions
+    group by account
+    having sum(amount) > 10000
+)
+select u.name, t.balance
+from users u
+     inner join t on (t.account = u.account)
+;
 
 
 # MySQL
