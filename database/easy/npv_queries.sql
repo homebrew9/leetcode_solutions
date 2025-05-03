482f4e13965d54cce77f26f1f42152852ad45da6
Pratyush Joshi <dmodeler@gmail.com>
2025-05-02 23:49:03 -0400

    Added MySQL solution for Database - NPV queries

--- database/easy/npv_queries.sql
+++ database/easy/npv_queries.sql
@@ -8,7 +8,11 @@
 
 
 # MySQL
-
+# Write your MySQL query statement below
+select q.id, q.year, coalesce(n.npv, 0) as npv
+  from queries q
+       left outer join npv n on (n.id = q.id and n.year = q.year)
+;
 
 # Pandas
 import pandas as pd
