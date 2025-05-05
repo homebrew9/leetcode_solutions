4a569330318ffb87684b04085c8a5ec442cd11fd
Pratyush Joshi <dmodeler@gmail.com>
2025-05-04 23:48:30 -0400

    Added solution for Count anagrams

---
+++ algorithms/hard/count_anagrams.py
@@ -0,0 +1,37 @@
+from collections import Counter
+import math
+class Solution:
+    def countAnagrams(self, s: str) -> int:
+        def perm(word):
+            size = len(word)
+            cntr = Counter(word)
+            res = math.factorial(size)
+            for i, v in cntr.items():
+                if v > 1:
+                    res = res // math.factorial(v)
+            return res
+        
+        cnt = 1
+        MOD = 10**9 + 7
+        for word in s.split():
+            cnt = cnt * perm(word)
+        return cnt % MOD
+
+
+
+
+
+
+# Main section
+for s in [
+            'too hot',
+            'aa',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.countAnagrams(s)
+    print(f'r = {r}')
+    print('========================')
+
+
+
