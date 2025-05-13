04c1578e2c5c514009de0e9c42b8f786654e3e69
Pratyush Joshi <dmodeler@gmail.com>
2025-05-12 22:19:43 -0400

    Added solution for Three consecutive odds

---
+++ algorithms/easy/three_consecutive_odds.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odd_count = 0
+        for n in arr:
+            if n % 2 == 1:
+                odd_count += 1
+                if odd_count == 3:
+                    return True
+            else:
+                odd_count = 0
+        return False
+
+# Main section
+for arr in [
+              [2,6,4,1],
+              [1,2,34,3,4,5,7,23,12],
+           ]:
+    print(f'arr = {arr}')
+    sol = Solution()
+    r = sol.threeConsecutiveOdds(arr)
+    print(f'r = {r}')
+    print('============================')
+
