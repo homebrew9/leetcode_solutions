f0c35a0c83d26329d5090ed9a515174d7505a9ac
Pratyush Joshi <dmodeler@gmail.com>
2025-05-14 19:24:53 -0400

    Added solution for High five

--- algorithms/easy/high_five.py
+++ algorithms/easy/high_five.py
@@ -27,7 +27,21 @@ class Solution:
         return sorted(res)
     def highFive_2(self, items: List[List[int]]) -> List[List[int]]:
         return [[p[0][0], sum([n for _, n in p])//5] for p in [list(g)[-5:] for k, g in groupby(sorted(items), key=lambda x: x[0])]]
-    
+    def highFive_3(self, items: List[List[int]]) -> List[List[int]]:
+        res = list()
+        count = 0
+        total, prev_id = None, None
+        for id, score in sorted(items, key=lambda x: (x[0], -x[1])):
+            if id != prev_id:
+                total = score
+                count = 1
+            else:
+                total += score
+                count += 1
+                if count == 5:
+                    res.append([id, total//5])
+            prev_id = id
+        return res    
 
 # Main section
 for items in [
@@ -39,8 +53,12 @@ for items in [
     r = sol.highFive(items)
     r1 = sol.highFive_1(items)
     r2 = sol.highFive_2(items)
+    r3 = sol.highFive_3(items)
     print(f'r     = {r}')
     print(f'r1    = {r1}')
     print(f'r2    = {r2}')
+    print(f'r3    = {r3}')
     print('============================')
+
+
 
