1c80c80e3bcf391de7a241f53219d286421d74e5
Pratyush Joshi <dmodeler@gmail.com>
2025-05-17 21:55:51 -0400

    Added solution for Minimum deletions for at most k distinct characters

---
+++ algorithms/easy/minimum_deletions_for_at_most_k_distinct_characters.py
@@ -0,0 +1,50 @@
+from collections import Counter
+
+class Solution:
+    def minDeletion(self, s: str, k: int) -> int:
+        cntr = Counter(s)
+        arr = sorted([(k, v) for k, v in cntr.items()], key=lambda x: (-x[1], x[0]))
+        return sum([x[1] for x in arr[k:]])
+
+# Main section
+for s, k in [
+               ('abc', 2),
+               ('aabb', 2),
+               ('yyyzz', 1),
+            ]:
+    print(f's, k = {s}, {k}')
+    sol = Solution()
+    r = sol.minDeletion(s, k)
+    print(f'r = {r}')
+    print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
