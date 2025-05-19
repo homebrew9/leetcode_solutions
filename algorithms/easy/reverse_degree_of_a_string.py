54f79c44030417827b3091ac9c1dc6360b6084a2
Pratyush Joshi <dmodeler@gmail.com>
2025-05-19 18:50:32 -0400

    Added solution for Reverse degree of a string

---
+++ algorithms/easy/reverse_degree_of_a_string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        res = 0
+        for i, v in enumerate(s):
+            res += (i + 1) * (123 - ord(v))
+        return res
+
+# Main section
+for s in [
+            'abc',
+            'zaza',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.reverseDegree(s)
+    print(f'r = {r}')
+    print('============================')
+
+
+
+
+
+
+
