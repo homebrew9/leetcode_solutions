9fdb77469e31b9541286351b80e1a8058ba8ec34
Pratyush Joshi <dmodeler@gmail.com>
2025-05-20 23:42:39 -0400

    Added PostgreSQL solution for Database - Find category recommendation pairs

--- database/hard/find_category_recommendation_pairs.sql
+++ database/hard/find_category_recommendation_pairs.sql
@@ -17,6 +17,26 @@ order by customer_count desc, category1, category2
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t (user_id, category) as (
+    select pp.user_id, pi.category
+      from ProductPurchases pp
+           inner join ProductInfo pi on (pi.product_id = pp.product_id)
+)
+select t1.category as category1,
+       t2.category as category2,
+       count(distinct t1.user_id) as customer_count
+  from t t1
+       inner join t t2 on (t2.user_id = t1.user_id and t1.category < t2.category)
+ group by t1.category, t2.category
+having count(distinct t1.user_id) >= 3
+order by customer_count desc, category1, category2
+;
+
+
+
+
+
 
 
 -- SQL Server
