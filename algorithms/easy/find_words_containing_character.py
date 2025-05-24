c999642a6bae09149efefe978ab4df28f03677e2
Pratyush Joshi <dmodeler@gmail.com>
2025-05-24 01:09:01 -0400

    Added solution for Find words containing character

--- algorithms/easy/find_words_containing_character.py
+++ algorithms/easy/find_words_containing_character.py
@@ -3,6 +3,12 @@ from typing import List
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         return [i for i, v in enumerate(words) if x in v]
+    def findWordsContaining_1(self, words: List[str], x: str) -> List[int]:
+        res = list()
+        for i, word in enumerate(words):
+            if x in word:
+                res.append(i)
+        return res
 
 # Main section
 for words, x in [
@@ -13,6 +19,26 @@ for words, x in [
     print(f'words, x = {words}, {x}')
     sol = Solution()
     r = sol.findWordsContaining(words, x)
-    print(f'r = {r}')
+    r1 = sol.findWordsContaining_1(words, x)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
