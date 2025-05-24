87567a29208efad7bc78ddfd73fe5e4b41aa966f
Pratyush Joshi <dmodeler@gmail.com>
2025-05-24 00:57:36 -0400

    Added solution for Fill a special grid

---
+++ algorithms/medium/fill_a_special_grid.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class Solution:
+    def specialGrid(self, n: int) -> List[List[int]]:
+        def solve(r, c, side):
+            if side == 1:
+                grid[r][c] = self.res
+                self.res += 1
+                return
+            solve(r, c + side//2, side//2)
+            solve(r + side//2, c + side//2, side//2)
+            solve(r + side//2, c, side//2)
+            solve(r, c, side//2)
+        rows = 2**n
+        cols = 2**n
+        grid = [[None for _ in range(cols)] for _ in range(rows)]
+        self.res = 0
+        solve(0, 0, 2**n)
+        return grid
+ 
+# Main section
+for n in [
+            0,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.specialGrid(n)
+    print(f'r = {r}')
+    print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
