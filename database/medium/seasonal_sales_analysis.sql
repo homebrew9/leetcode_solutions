16f4dfae1d922a8c254cee99572e285533bc3fe1
Pratyush Joshi <dmodeler@gmail.com>
2025-05-26 16:51:18 -0400

    Added Oracle solution for Database - Seasonal sales analysis

---
+++ database/medium/seasonal_sales_analysis.sql
@@ -0,0 +1,42 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+with t as (
+    select case when to_char(s.sale_date, 'mon') in ('dec','jan','feb') then 'Winter'
+                when to_char(s.sale_date, 'mon') in ('mar','apr','may') then 'Spring'
+                when to_char(s.sale_date, 'mon') in ('jun','jul','aug') then 'Summer'
+                else 'Fall'
+        end as season,
+        p.category,
+        sum(s.quantity) as total_quantity, sum(s.quantity * s.price) as total_revenue
+    from sales s
+        inner join products p on (p.product_id = s.product_id)
+    group by case when to_char(s.sale_date, 'mon') in ('dec','jan','feb') then 'Winter'
+                  when to_char(s.sale_date, 'mon') in ('mar','apr','may') then 'Spring'
+                  when to_char(s.sale_date, 'mon') in ('jun','jul','aug') then 'Summer'
+                  else 'Fall'
+             end, p.category
+),
+t1 as (
+    select season, category, total_quantity, total_revenue,
+           dense_rank() over (partition by season order by total_quantity desc, total_revenue desc) as drnk
+    from t
+)
+select season, category, total_quantity, total_revenue
+  from t1
+ where drnk = 1
+ order by season
+;
+
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+
