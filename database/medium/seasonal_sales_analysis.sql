7165ae3f1dd7cbd96a30ccff1838f718eb4d3e92
Pratyush Joshi <dmodeler@gmail.com>
2025-05-26 16:53:10 -0400

    Added SQL Server solution for Database - Seasonal sales analysis

--- database/medium/seasonal_sales_analysis.sql
+++ database/medium/seasonal_sales_analysis.sql
@@ -59,6 +59,34 @@ select season, category, total_quantity, total_revenue
 
 
 -- SQL Server
+/* Write your T-SQL query statement below */
+with t as (
+    select case when convert(varchar(3), s.sale_date, 7) in ('dec','jan','feb') then 'Winter'
+                when convert(varchar(3), s.sale_date, 7) in ('mar','apr','may') then 'Spring'
+                when convert(varchar(3), s.sale_date, 7) in ('jun','jul','aug') then 'Summer'
+                else 'Fall'
+        end as season,
+        p.category,
+        sum(s.quantity) as total_quantity, sum(s.quantity * s.price) as total_revenue
+    from sales s
+        inner join products p on (p.product_id = s.product_id)
+    group by case when convert(varchar(3), s.sale_date, 7) in ('dec','jan','feb') then 'Winter'
+                  when convert(varchar(3), s.sale_date, 7) in ('mar','apr','may') then 'Spring'
+                  when convert(varchar(3), s.sale_date, 7) in ('jun','jul','aug') then 'Summer'
+                  else 'Fall'
+             end, p.category
+),
+t1 as (
+    select season, category, total_quantity, total_revenue,
+           dense_rank() over (partition by season order by total_quantity desc, total_revenue desc) as drnk
+    from t
+)
+select season, category, total_quantity, total_revenue
+  from t1
+ where drnk = 1
+ order by season
+;
+
 
 
 # MySQL
