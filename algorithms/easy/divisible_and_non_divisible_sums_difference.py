25d79af8e23b5694b003a73e8738a8db394a9fac
Pratyush Joshi <dmodeler@gmail.com>
2025-05-27 21:53:06 -0400

    Added solution for Divisible and non-divisible sums difference

---
+++ algorithms/easy/divisible_and_non_divisible_sums_difference.py
@@ -0,0 +1,40 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        res = 0
+        for i in range(1, n+1):
+            if i % m != 0:
+                res += i
+            else:
+                res -= i
+        return res
+
+# Main section
+for n, m in [
+               (10, 3),
+               (5, 6),
+               (5, 1),
+               (997, 13),
+               (1000, 1),
+            ]:
+    print(f'n, m = {n}, {m}')
+    sol = Solution()
+    r = sol.differenceOfSums(n, m)
+    print(f'r  = {r}')
+    print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
