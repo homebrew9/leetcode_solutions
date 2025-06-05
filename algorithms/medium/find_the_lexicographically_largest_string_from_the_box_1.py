8544f5b44f29d62bb836f70dba9adbf3bbe1627f
Pratyush Joshi <dmodeler@gmail.com>
2025-06-04 23:41:15 -0400

    Added solution for Find the lexicographically largest string from the box I

---
+++ algorithms/medium/find_the_lexicographically_largest_string_from_the_box_1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        N = len(word)
+        span = N - numFriends + 1
+        res = ''
+        for i in range(N):
+            chunk = word[i:i+span]
+            res = max(res, chunk)
+        return res
+
+# Main section
+for word, numFriends in [
+                           ('dbca', 2),
+                           ('gggg', 4),
+                           ('gh', 1),
+                        ]:
+    print(f'word, numFriends = {word}, {numFriends}')
+    sol = Solution()
+    r = sol.answerString(word, numFriends)
+    print(f'r = {r}')
+    print('=======================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
