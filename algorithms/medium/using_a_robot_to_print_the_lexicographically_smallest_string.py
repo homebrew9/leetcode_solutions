1ec87f707aa3ba60d3442eaf759e44c37be98451
Pratyush Joshi <dmodeler@gmail.com>
2025-06-06 23:56:27 -0400

    Added solution for Using a robot to print the lexicographically smallest string

---
+++ algorithms/medium/using_a_robot_to_print_the_lexicographically_smallest_string.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        N = len(s)
+        hsh = defaultdict(int)
+        for ch in s:
+            hsh[ch] += 1
+        stack = list()
+        res = ''
+        for ch in s:
+            stack.append(ch)
+            hsh[ch] -= 1
+            if hsh[ch] == 0:
+                del hsh[ch]
+            while stack and (len(hsh) == 0 or stack[-1] <= min(hsh)):
+                res += stack.pop()
+        return res
+
+# Main section
+for s in [
+            'zza',
+            'bac',
+            'bdda',
+            'bydizfve',
+            'bzeyxf',
+            'vzhofnpo',
+            'mmuqezwmomeplrtskz',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.robotWithString(s)
+    print(f'r = {r}')
+    print('=============')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
