d787348fc55dfbbd9ad85a75c75370c250e59659
Pratyush Joshi <dmodeler@gmail.com>
2025-06-08 18:32:28 -0400

    Added Oracle solution for Database - Find books with no available copies

---
+++ database/easy/find_books_with_no_available_copies.sql
@@ -0,0 +1,30 @@
+-- Oracle
+/* Write your PL/SQL query statement below */
+with t as (
+    select lb.book_id, lb.title, lb.author, lb.genre, lb.publication_year,
+           lb.total_copies, count(*) as current_borrowers
+    from library_books lb
+         inner join borrowing_records br on (br.book_id = lb.book_id and br.return_date is null)
+    group by lb.book_id, lb.title, lb.author, lb.genre, lb.publication_year, lb.total_copies
+    having lb.total_copies = count(*)
+)
+select book_id, title, author, genre, publication_year, current_borrowers
+  from t
+ order by current_borrowers desc, title
+;
+
+
+
+
+
+-- PostgreSQL
+
+
+-- SQL Server
+
+
+# MySQL
+
+
+# Pandas
+
