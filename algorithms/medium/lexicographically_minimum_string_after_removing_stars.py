d7284a5183b0efa011c4f60194a605cea884e921
Pratyush Joshi <dmodeler@gmail.com>
2025-06-07 23:56:08 -0400

    Added solution for Lexicographically minimum string after removing stars

---
+++ algorithms/medium/lexicographically_minimum_string_after_removing_stars.py
@@ -0,0 +1,56 @@
+class Solution:
+    def clearStars(self, s: str) -> str:
+        arr = list(s)
+        chars = [[] for _ in range(26)]
+        for i, v in enumerate(arr):
+            if v == '*':
+                for j in range(26):
+                    if len(chars[j]) > 0:
+                        ind = chars[j].pop()
+                        arr[ind] = '*'
+                        break
+            else:
+                ind = ord(v) - ord('a')
+                chars[ind].append(i)
+        return ''.join([ch for ch in arr if ch != '*'])
+
+# Main section
+for s in [
+            'aaba*',
+            'abc',
+            'yxzkrclb*foemxxqbzufnajeqaj**yzrzvvfdctdhp***pnavby*osklnzkqdzultlszdgi**tilujoianyehwimvdtip*vbmazsyuqqyl**drcd*',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.clearStars(s)
+    print(f'r = {r}')
+    print('=============')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
