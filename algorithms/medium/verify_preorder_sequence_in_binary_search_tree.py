7ef26ede7e0e9c85c70135d1207375ae4151e72e
Pratyush Joshi <dmodeler@gmail.com>
2025-06-09 18:49:44 -0400

    Added solution for Verify preorder sequence in Binary Search Tree

---
+++ algorithms/medium/verify_preorder_sequence_in_binary_search_tree.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        lower_bound = float('-inf')
+        stack = list()
+        for n in preorder:
+            if n <= lower_bound:
+                return False
+            while len(stack) > 0 and n > stack[-1]:
+                lower_bound = stack.pop()
+            stack.append(n)
+        return True
+
+# Main section
+for preorder in [
+                   [5,2,1,3,6],
+                   [5,2,6,1,3],
+                   [4,2,1,3,6,5,7],
+                   [8,4,2,1,3,6,5,7,12,10,9,11,14,13,15],
+                   [5,2,1,10,6],
+                ]:
+    print(f'preorder = {preorder}')
+    sol = Solution()
+    r = sol.verifyPreorder(preorder)
+    print(f'r = {r}')
+    print('==================')
+
+
+
+
+
+
+
+
+
