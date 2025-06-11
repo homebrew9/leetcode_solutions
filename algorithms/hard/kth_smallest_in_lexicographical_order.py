7b1514e32c102381a126aec4dde0c6053ef79f09
Pratyush Joshi <dmodeler@gmail.com>
2025-06-10 22:58:54 -0400

    Added solution for K-th smallest in lexicographical order

---
+++ algorithms/hard/kth_smallest_in_lexicographical_order.py
@@ -0,0 +1,50 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        curr = 1
+        k -= 1
+
+        while k > 0:
+            step = self._count_steps(n, curr, curr + 1)
+            # If the steps are less than or equal to k, we skip this prefix's subtree
+            if step <= k:
+                # Move to the next prefix and decrease k by the number of steps we skip
+                curr += 1
+                k -= step
+            else:
+                # Move to the next level of the tree and decrement k by 1
+                curr *= 10
+                k -= 1
+
+        return curr
+
+    # To count how many numbers exist between prefix1 and prefix2
+    def _count_steps(self, n, prefix1, prefix2):
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n + 1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        return steps
+
+# Main section
+for n, k in [
+               (13, 2),
+               (1, 1),
+               (1000000000, 93990919),
+               (372839276, 46379107),
+            ]:
+    print(f'n, k = {n}, {k}')
+    sol = Solution()
+    r = sol.findKthNumber(n, k)
+    print(f'r  = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
