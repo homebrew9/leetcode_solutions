cd6d6428197bdfed68e384192ae27d6d5b0fac7d
Pratyush Joshi <dmodeler@gmail.com>
2025-06-15 21:55:37 -0400

    Added solution for Maximum difference between increasing elements, version 1

---
+++ algorithms/easy/maximum_difference_between_increasing_elements_v1.py
@@ -0,0 +1,57 @@
+from typing import List
+
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        min_val = float('inf')
+        max_diff = -1
+        for n in nums:
+            if n < min_val:
+                min_val = n
+            elif n > min_val:
+                max_diff = max(max_diff, n - min_val)
+        return max_diff
+
+# Main section
+for nums in [
+               [7,1,5,4],
+               [9,4,3,2],
+               [1,5,2,10],
+               [1,1,1,1],
+               [1,2,3,9],
+               [9,3,2,1],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.maximumDifference(nums)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
