dcf679120ebc106e8882ad1c6f4bd62a596fd188
Pratyush Joshi <dmodeler@gmail.com>
2025-06-19 23:52:17 -0400

    Added solution for Exclusive time of functions

--- algorithms/medium/exclusive_time_of_functions.py
+++ algorithms/medium/exclusive_time_of_functions.py
@@ -1,3 +1,11 @@
+# =========================================================================================
+# Both functions below use a stack. In the first function, we calculate the exclusive time
+# of a function when it ends, and immediately subtract that time from the function at the
+# top of stack. For example, if logs = ['0:start:0','1:start:4','1:end:7','0:end:10']
+# then function 1 ran for 4 seconds [4,7]; we subtract 4 from function 0. Then
+# function 0 ran for [0,10] minus exclusive time for function 1 i.e. 11 - 4 = 7 seconds.
+# In the second function, we update function 0 as soon as we start function 1.
+# =========================================================================================
 from typing import List
 
 class Solution:
@@ -17,6 +25,28 @@ class Solution:
                     ind = stack[-1][0]
                     res[ind] -= diff
         return res
+    def exclusiveTime_1(self, n: int, logs: List[str]) -> List[int]:
+        res = [0 for _ in range(n)]
+        prev_ts = None
+        stack = list()
+        for entry in logs:
+            id, action, ts = entry.split(':')
+            id, ts = int(id), int(ts)
+            if len(stack) == 0:
+                stack.append([id, 1])
+            else:
+                diff = max(0, ts - prev_ts - 1)
+                stack[-1][1] += diff
+                if action == 'end':
+                    idx, time = stack.pop()
+                    if ts == prev_ts:
+                        res[idx] += time
+                    else:
+                        res[idx] += time + 1
+                else:
+                    stack.append([id, 1])
+            prev_ts = ts
+        return res
 
 # Main section
 for n, logs in [
@@ -27,7 +57,9 @@ for n, logs in [
     print(f'n, logs = {n}, {logs}')
     sol = Solution()
     r = sol.exclusiveTime(n, logs)
-    print(f'r = {r}')
+    r1 = sol.exclusiveTime_1(n, logs)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('===========================')
 
 
