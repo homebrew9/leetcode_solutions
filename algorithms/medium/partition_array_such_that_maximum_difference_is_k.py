f3991bdb3a045105fecff0fb70ca2497f3b7ee7c
Pratyush Joshi <dmodeler@gmail.com>
2025-06-19 23:39:24 -0400

    Added solution for Partition array such that maximum difference is k

---
+++ algorithms/medium/partition_array_such_that_maximum_difference_is_k.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        nums.sort()
+        res = 1
+        i, j = 0, 0
+        while j < N:
+            if nums[j] - nums[i] > k:
+                i = j
+                res += 1
+            j += 1
+        return res
+
+# Main section
+for nums, k in [
+                  ([3,6,1,2,5], 2),
+                  ([1,2,3], 1),
+                  ([2,2,4,5], 0),
+               ]:
+    print(f'nums, k = {nums}, {k}')
+    sol = Solution()
+    r = sol.partitionArray(nums, k)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
