159db29b09dd813020628cc83201856f9fc6dd14
Pratyush Joshi <dmodeler@gmail.com>
2025-06-21 23:41:14 -0400

    Added solution for Minimum deletions to make string k-special

---
+++ algorithms/medium/minimum_deletions_to_make_string_k_special.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        cnt = defaultdict(int)
+        for c in word:
+            cnt[c] += 1
+        res = len(word)
+        for a in cnt.values():
+            deleted = 0
+            for b in cnt.values():
+                if a > b:
+                    deleted += b
+                elif b > a + k:
+                    deleted += b - (a + k)
+            res = min(res, deleted)
+        return res
+
+# Main section
+for word, k in [
+                  ('aabcaba', 0),
+                  ('dabdcbdcdcd', 2),
+                  ('aaabaaa', 2),
+               ]:
+    print(f'word, k = {word}, {k}')
+    sol = Solution()
+    r = sol.minimumDeletions(word, k)
+    print(f'r = {r}')
+    print('=======================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
