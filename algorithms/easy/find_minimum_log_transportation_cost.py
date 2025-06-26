ce7bd9ba5f83140eb6c7ca8f8d672f43debef3b7
Pratyush Joshi <dmodeler@gmail.com>
2025-06-26 19:08:46 -0400

    Added solution for Find minimum log transportation cost

---
+++ algorithms/easy/find_minimum_log_transportation_cost.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minCuttingCost(self, n: int, m: int, k: int) -> int:
+        if n > k:
+            return (n - k) * k
+        if m > k:
+            return (m - k) * k
+        return 0
+
+# Main section
+for n, m, k in [
+                  (6, 5, 5),
+                  (4, 4, 6),
+               ]:
+    print(f'n, m, k = {n}, {m}, {k}')
+    sol = Solution()
+    r = sol.minCuttingCost(n, m, k)
+    print(f'r = {r}')
+    print('=======================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
