98395e71d5e3e4164ca38370f3792d1385eff956
Pratyush Joshi <dmodeler@gmail.com>
2025-06-28 19:24:11 -0400

    Added solution for Count odd letters from number

---
+++ algorithms/easy/count_odd_letters_from_number.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+class Solution:
+    def countOddLetters(self, n: int) -> int:
+        hsh = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+               6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+        cntr = defaultdict(int)
+        while n > 0:
+            q, r = divmod(n, 10)
+            for ch in hsh[r]:
+                cntr[ch] += 1
+            n = q
+        return sum([v % 2 == 1 for v in cntr.values()])
+
+# Main section
+for n in [
+            41,
+            20,
+            1000000000,
+            899834798,
+            173720093,
+            3599,
+            99832382,
+            217,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.countOddLetters(n)
+    print(f'r = {r}')
+    print('=======================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
