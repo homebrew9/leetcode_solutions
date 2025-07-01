55d751af66e2cf96a7bd69ccbf64da02051ea31f
Pratyush Joshi <dmodeler@gmail.com>
2025-06-30 22:06:41 -0400

    Added solution for Find the original typed string I

--- algorithms/easy/find_the_original_typed_string_1.py
+++ algorithms/easy/find_the_original_typed_string_1.py
@@ -10,6 +10,13 @@ class Solution:
             right += 1
         res += right - 1 - left
         return res + 1
+    def possibleStringCount_1(self, word: str) -> int:
+        N = len(word)
+        res = 1
+        for i in range(1, N):
+            if word[i-1] == word[i]:
+                res += 1
+        return res
 
 # Main section
 for word in [
@@ -20,7 +27,15 @@ for word in [
     print(f'word = {word}')
     sol = Solution()
     r = sol.possibleStringCount(word)
+    r1 = sol.possibleStringCount_1(word)
     print(f'r    = {r}')
+    print(f'r1   = {r1}')
     print('=======================')
+
+
+
+
+
+
 
 
