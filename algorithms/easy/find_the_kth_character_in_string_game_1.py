6fc94ad0ea3ba97adc140e9300dba011c54b933c
Pratyush Joshi <dmodeler@gmail.com>
2025-07-02 21:44:25 -0400

    Added solution for Find the k-th character in string game I

--- algorithms/easy/find_the_kth_character_in_string_game_1.py
+++ algorithms/easy/find_the_kth_character_in_string_game_1.py
@@ -2,6 +2,7 @@
 # I used Brute Force. However, check the solutions that use bit count technique.
 # How is bit count relevant here? Check solutions by lee215 or Spaulding.
 #
+import string
 class Solution:
     def kthCharacter(self, k: int) -> str:
         hsh = {chr(97+i) : chr(97+(i+1)%26) for i in range(26)}
@@ -9,6 +10,13 @@ class Solution:
         while len(s) < k:
             s += ''.join([hsh[ch] for ch in s])
         return s[k-1]
+    def kthCharacter_1(self, k: int) -> str:
+        chars = string.ascii_lowercase
+        word = 'a'
+        while len(word) < k:
+            word += ''.join([chars[(chars.index(ch)+1)%26] for ch in word])
+        return word[k-1]
+
 
 # Main section
 for k in [
@@ -22,10 +30,22 @@ for k in [
             473,
             500,
          ]:
-    print(f'k = {k}')
+    print(f'k  = {k}')
     sol = Solution()
     r = sol.kthCharacter(k)
-    print(f'r = {r}')
+    r1 = sol.kthCharacter_1(k)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('===================')
+
+
+
+
+
+
+
+
+
+
 
 
