01133ec15fd821d29f0d016ff0d8769fb64f02c0
Pratyush Joshi <dmodeler@gmail.com>
2025-07-05 23:45:01 -0400

    Added solution for Find lucky integer in an array

---
+++ algorithms/easy/find_lucky_integer_in_an_array.py
@@ -0,0 +1,40 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        cntr = Counter(arr)
+        for k in sorted(cntr, reverse=True):
+            if cntr[k] == k:
+                return k
+        return -1
+
+# Main section
+for arr in [
+              [2,2,3,4],
+              [1,2,2,3,3,3],
+              [2,2,2,3,3],
+           ]:
+    print(f'arr = {arr}')
+    sol = Solution()
+    r = sol.findLucky(arr)
+    print(f'r  = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
