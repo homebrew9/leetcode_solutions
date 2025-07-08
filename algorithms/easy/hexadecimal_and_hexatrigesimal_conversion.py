4ac6d9777a48bf35a2f7431b071fbdb4d7d9ca79
Pratyush Joshi <dmodeler@gmail.com>
2025-07-07 22:43:36 -0400

    Added solution for Hexadecimal and hexatrigesimal conversion

---
+++ algorithms/easy/hexadecimal_and_hexatrigesimal_conversion.py
@@ -0,0 +1,31 @@
+class Solution:
+    def concatHex36(self, n: int) -> str:
+        def get_hex_repr(n, base):
+            res = ''
+            while n > 0:
+                q, r = divmod(n, base)
+                res = hsh[r] + res
+                n = q
+            return res
+        hsh = {i: str(i) if i < 10 else chr(i + 55) for i in range(36)}
+        return get_hex_repr(n*n, 16) + get_hex_repr(n*n*n, 36)
+
+# Main section
+for n in [
+            13,
+            36,
+            1,
+            500,
+            1000,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.concatHex36(n)
+    print(f'r = {r}')
+    print('===================')
+
+
+
+
+
+
