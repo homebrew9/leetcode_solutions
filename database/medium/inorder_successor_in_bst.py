6372288763ea23c695a4b2b2b97580a0ee4c5686
Pratyush Joshi <dmodeler@gmail.com>
2025-07-09 22:47:02 -0400

    Added solution for Inorder successor in BST

---
+++ database/medium/inorder_successor_in_bst.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                self.res.append(node)
+                if node == p:
+                    self.ind = len(self.res) - 1
+                dfs(node.right)
+        self.ind = None
+        self.res = list()
+        dfs(root)
+        return None if self.ind + 1 >= len(self.res) else self.res[self.ind + 1]
+
+# Main section
+# [2,1,3], 1
+# [5,3,6,2,4,null,null,1], 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
