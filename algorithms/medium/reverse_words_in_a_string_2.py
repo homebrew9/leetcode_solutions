3c810d7216de120835e3ff7c51f671e76bf01a90
Pratyush Joshi <dmodeler@gmail.com>
2025-07-12 23:50:59 -0400

    Added solution for Reverse words in a string II

---
+++ algorithms/medium/reverse_words_in_a_string_2.py
@@ -0,0 +1,58 @@
+from typing import List
+
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse_elements(i, j):
+            left, right = i, j
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        # 1) Handle corner case first
+        if ' ' not in s:
+            print(s)
+            return
+        N = len(s)
+        # 2) Reverse individual words
+        left, right = 0, 0
+        while right < N:
+            if s[right] == ' ':
+                reverse_elements(left, right - 1)
+                left = right + 1
+            right += 1
+        reverse_elements(left, right - 1)
+        # 3) Finally, reverse the entire string
+        reverse_elements(0, N - 1)
+        print(s)
+
+# Main section
+for s in [
+            ['t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e'],
+            ['a'],
+            ['h','e','l','l','o',' ','w','o','r','l','d'],
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.reverseWords(s)
+    print(f'r = {r}')
+    print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
