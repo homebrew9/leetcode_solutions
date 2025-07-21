423b313c0a01247e40b5a416d0c1038b49722e5e
Pratyush Joshi <dmodeler@gmail.com>
2025-07-20 23:29:20 -0400

    Added solution for Check divisibility by digit sum and product

---
+++ algorithms/easy/check_divisibility_by_digit_sum_and_product.py
@@ -0,0 +1,53 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        def num_iter(n, op):
+            res = 0 if op == 'sum' else 1
+            while n > 0:
+                q, r = divmod(n, 10)
+                if op == 'sum':
+                    res += r
+                elif op == 'prod':
+                    res *= r
+                n = q
+            return res
+        digit_sum = num_iter(n, 'sum')
+        digit_product = num_iter(n, 'prod')
+        return n % (digit_sum + digit_product) == 0
+
+# Main section
+for n in [
+            99,
+            23,
+            12345,
+            10101,
+            1000,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.checkDivisibility(n)
+    print(f'r = {r}')
+    print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
