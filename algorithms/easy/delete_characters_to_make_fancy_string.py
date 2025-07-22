ae5c5ca7fbf0bb5059cb42d696d57c41b924e65f
Pratyush Joshi <dmodeler@gmail.com>
2025-07-21 22:39:27 -0400

    Added solution for Delete characters to make fancy string

---
+++ algorithms/easy/delete_characters_to_make_fancy_string.py
@@ -0,0 +1,45 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res = ''
+        for i, v in enumerate(s):
+            if i >= 2:
+                if v == res[-1] and v == res[-2]:
+                    continue
+            res += v
+        return res
+
+# Main section
+for s in [
+            'leeetcode',
+            'aaabaaaa',
+            'aab',
+         ]:
+    print(f's  = {s}')
+    sol = Solution()
+    r = sol.makeFancyString(s)
+    print(f'r  = {r}')
+    print('============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
