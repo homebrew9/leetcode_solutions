2988d7e391febfaf5de6c5badc349dba66522095
Pratyush Joshi <dmodeler@gmail.com>
2025-07-22 23:48:42 -0400

    Added solution for Maximum erasure value

--- algorithms/medium/maximum_erasure_value.py
+++ algorithms/medium/maximum_erasure_value.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 class Solution:
@@ -20,6 +21,30 @@ class Solution:
             res = max(res, curr)
             right += 1
         return res
+    def maximumUniqueSubarray_1(self, nums: List[int]) -> int:
+        # This algorithm is highly inefficient for large arrays with all distinct elements.
+        # For example, it throws TLE for the test case: list(range(1, 10001))
+        # The reason is that the check: "set(hsh.values()) == {1}" gets very expensive as
+        # the dictionary grows to 10K unique keys. The other method uses an efficient algorithm.
+        # It uses a set that has O(1) lookup. Whenever there is a potential conflict, it keeps
+        # removing elements from the left until we have removed the conflicting integer.
+        N = len(nums)
+        hsh = defaultdict(int)
+        curr = 0
+        res = float('-inf')
+        left, right = 0, 0
+        while right < N:
+            curr += nums[right]
+            hsh[nums[right]] += 1
+            while not ( set(hsh.values()) == {1} ):
+                curr -= nums[left]
+                hsh[nums[left]] -= 1
+                if hsh[nums[left]] == 0:
+                    del hsh[nums[left]]
+                left += 1
+            res = max(res, curr)
+            right += 1
+        return res
 
 # Main section
 for nums in [
@@ -30,7 +55,9 @@ for nums in [
     print(f'nums = {nums}')
     sol = Solution()
     r = sol.maximumUniqueSubarray(nums)
-    print(f'r = {r}')
+    print(f'r  = {r}')
+    r1 = sol.maximumUniqueSubarray_1(nums)
+    print(f'r1 = {r1}')
     print('============================')
 
 
