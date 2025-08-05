53fce0f8b0cc576785c9f1042c0b53363f8c9600
Pratyush Joshi <dmodeler@gmail.com>
2025-08-04 23:38:49 -0400

    Added solution for Database - Find stores with inventory imbalance

--- database/medium/find_stores_with_inventory_imbalance.sql
+++ database/medium/find_stores_with_inventory_imbalance.sql
@@ -111,4 +111,46 @@ select s.store_id, s.store_name, s.location,
 
 
 # Pandas
+import pandas as pd
+
+def find_inventory_imbalance(stores: pd.DataFrame, inventory: pd.DataFrame) -> pd.DataFrame:
+    valid_store_ids = ( inventory
+                       .groupby('store_id', as_index=0)
+                       .agg(n_distinct_products=('product_name', 'nunique'))
+                       .query('n_distinct_products >= 3')['store_id']
+                      )
+    df = ( inventory[inventory['store_id'].isin(valid_store_ids)]
+          .sort_values(by=['store_id','price'])
+         )
+    df['drnk_cheapest'] = ( df
+                           .groupby('store_id', as_index=0)['price']
+                           .rank(method='dense')
+                          )
+    df['drnk_most_expensive'] = ( df
+                                 .groupby('store_id', as_index=0)['price']
+                                 .rank(method='dense', ascending=False)
+                                )
+    df_cheapest = df.query('drnk_cheapest == 1')[['store_id','product_name','quantity']]
+    df_most_expensive = df.query('drnk_most_expensive == 1')[['store_id','product_name','quantity']]
+    df1 = ( df_cheapest
+           .merge(df_most_expensive, how='inner', on='store_id')
+           .query('quantity_x > quantity_y')
+           .merge(stores, how='inner', on='store_id')
+           .rename(columns={'product_name_x': 'cheapest_product', 'product_name_y': 'most_exp_product'})
+          )
+    df1['imbalance_ratio'] = round(df1['quantity_x'] / df1['quantity_y'], 2)
+    return ( df1[['store_id','store_name','location','most_exp_product','cheapest_product','imbalance_ratio']]
+            .sort_values(by=['imbalance_ratio','store_name'], ascending=[False,True])
+           )
+
+
+
+
+
+
+
+
+
+
+
 
