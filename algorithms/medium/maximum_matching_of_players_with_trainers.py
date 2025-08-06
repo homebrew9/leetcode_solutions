9c7610df3abfa90f45b79b590a56e248b7ee222c
Pratyush Joshi <dmodeler@gmail.com>
2025-08-05 23:51:34 -0400

    Added solution for Maximum matching of players with trainers

---
+++ algorithms/medium/maximum_matching_of_players_with_trainers.py
@@ -0,0 +1,54 @@
+from typing import List
+
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        m, n = len(players), len(trainers)
+        i = j = count = 0
+
+        while i < m and j < n:
+            while j < n and players[i] > trainers[j]:
+                j += 1
+            if j < n:
+                count += 1
+            i += 1
+            j += 1
+
+        return count
+
+# Main section
+for players, trainers in [
+                            ([4,7,9], [8,2,5,8]),
+                            ([1,1,1], [10]),
+                         ]:
+    print(f'players  = {players}')
+    print(f'trainers = {trainers}')
+    sol = Solution()
+    r = sol.matchPlayersAndTrainers(players, trainers)
+    print(f'r = {r}')
+    print('================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
