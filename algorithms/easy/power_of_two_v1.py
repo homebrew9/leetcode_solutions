9652272eb15cc495055374e3b9a081e9c5344172
Pratyush Joshi <dmodeler@gmail.com>
2025-08-08 23:42:54 -0400

    Added solution for Power of two, version 1

--- algorithms/easy/power_of_two_v1.py
+++ algorithms/easy/power_of_two_v1.py
@@ -3,6 +3,10 @@ class Solution:
         # Bit manipulation
         return n > 0 and n & (n-1) == 0
 
+    def isPowerOfTwo_1(self, n: int) -> bool:
+        # Another one without using loops/recursion
+        return n > 0 and int(bin(n)[2:][::-1]) == 1
+
 # Main section
 for n in [
             65536,
@@ -11,10 +15,12 @@ for n in [
             0,
             -256,
          ]:
-    print(f'n = {n}')
+    print(f'n  = {n}')
     sol = Solution()
     r = sol.isPowerOfTwo(n)
-    print(f'r = {r}')
+    r1 = sol.isPowerOfTwo_1(n)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('================')
 
 
