8b4ce10425e96b9b6013e6cd635e6cdcf0bc2075
Pratyush Joshi <dmodeler@gmail.com>
2025-08-17 22:55:59 -0400

    Added solution for Maximum 69 number

---
+++ algorithms/easy/maximum_69_number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = str(num)
+        for i in range(len(s)):
+            if s[i] == '6':
+                return int(s[:i] + '9' + s[i+1:])
+        return num
+
+# Main section
+for num in [
+              9669,
+              9996,
+              9999,
+           ]:
+    print(f'num = {num}')
+    sol = Solution()
+    r = sol.maximum69Number(num)
+    print(f'r   = {r}')
+    print('==============================')
+
+
+
+
+
+
+
+
