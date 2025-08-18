3436dea19ec2d3e705aff4de988341fcfa32f740
Pratyush Joshi <dmodeler@gmail.com>
2025-08-17 22:49:29 -0400

    Added solution for Trionic array I

---
+++ algorithms/easy/trionic_array_1.py
@@ -0,0 +1,48 @@
+from typing import List
+
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        N = len(nums)
+        changes = 0
+        trend = None
+        for i in range(1, N):
+            if nums[i] == nums[i - 1]:
+                return False
+            if i == 1:
+                if nums[i] < nums[i - 1]:
+                    return False
+                changes += 1
+                trend = 'I'
+            elif nums[i] < nums[i - 1]:
+                if trend == 'I':
+                    trend = 'D'
+                    changes += 1
+            elif nums[i] > nums[i - 1]:
+                if trend == 'D':
+                    trend = 'I'
+                    changes += 1
+            if changes > 3:
+                return False
+        return changes == 3
+
+# Main section
+for nums in [
+               [1,3,5,4,2,6],
+               [2,1,3],
+               [1,2,3],
+               [1,2,3,4,3,2,1,2,3,4,5],
+               [1,2,3,4,3,3,1,2,3,4,5],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.isTrionic(nums)
+    print(f'r = {r}')
+    print('==============================')
+
+
+
+
+
+
+
+
