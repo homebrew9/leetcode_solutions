3412acba445ed66c77ed777495f9ea96fc698adc
Pratyush Joshi <dmodeler@gmail.com>
2025-08-21 22:58:13 -0400

    Added solution for Find the minimum area to cover all ones I

---
+++ algorithms/medium/find_the_minimum_area_to_cover_all_ones_1.py
@@ -0,0 +1,54 @@
+from typing import List
+
+class Solution:
+    def minimumArea(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        min_row, min_col = float('inf'), float('inf')
+        max_row, max_col = float('-inf'), float('-inf')
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    min_row = min(min_row, r)
+                    min_col = min(min_col, c)
+                    max_row = max(max_row, r)
+                    max_col = max(max_col, c)
+        min_area = (max_row - min_row + 1) * (max_col - min_col + 1)
+        return min_area
+
+# Main section
+for grid in [
+               [[0,1,0],[1,0,1]],
+               [[1,0],[0,0]],
+            ]:
+    print(f'grid  = {grid}')
+    sol = Solution()
+    r = sol.minimumArea(grid)
+    print(f'r = {r}')
+    print('==============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
