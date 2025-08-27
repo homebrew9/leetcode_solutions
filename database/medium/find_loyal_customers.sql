e56a6b2e3a60d3600870d9a17ea5960ab81b700d
Pratyush Joshi <dmodeler@gmail.com>
2025-08-27 08:02:39 -0400

    Added Pandas solution for Database - Find loyal customers

--- database/medium/find_loyal_customers.sql
+++ database/medium/find_loyal_customers.sql
@@ -91,4 +91,42 @@ select distinct customer_id
 
 
 # Pandas
+import pandas as pd
+import numpy as np
+
+def find_loyal_customers(customer_transactions: pd.DataFrame) -> pd.DataFrame:
+    min_purchase_customers = ( customer_transactions
+                              .query('transaction_type == "purchase"')
+                              .groupby('customer_id', as_index=0)
+                              .size()
+                              .query('size >= 3')['customer_id']
+                             )
+    customer_transactions['transaction_date'] = pd.to_datetime(customer_transactions['transaction_date'])
+    df_activity = ( customer_transactions
+                   .groupby('customer_id', as_index=0)
+                   .agg(min_date=('transaction_date', 'min'), max_date=('transaction_date', 'max'))
+                  )
+    df_activity['diff'] = (df_activity['max_date'] - df_activity['min_date']).dt.days
+    min_activity_customers = df_activity.query('diff >= 30')['customer_id']
+    customer_transactions['is_refund'] = np.where(customer_transactions['transaction_type']=='refund',1,0)
+    df_refund = ( customer_transactions
+                 .groupby('customer_id', as_index=0)
+                 .agg(refund_trx=('is_refund', 'sum'), total_trx=('transaction_id', 'count'))
+                )
+    df_refund['refund_rate'] = df_refund['refund_trx'] / df_refund['total_trx']
+    refund_rate_customers = df_refund.query('refund_rate < 0.2')['customer_id']
+    return ( customer_transactions[
+               (customer_transactions['customer_id'].isin(min_purchase_customers)) &
+               (customer_transactions['customer_id'].isin(min_activity_customers)) &
+               (customer_transactions['customer_id'].isin(refund_rate_customers))
+             ][['customer_id']]
+            .drop_duplicates()
+            .sort_values('customer_id')
+           )
+
+
+
+
+
+
 
