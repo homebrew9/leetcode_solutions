d3628d84951c98084fc44bb081ee9ae95cf121ea
Pratyush Joshi <dmodeler@gmail.com>
2025-08-27 08:01:20 -0400

    Added PostgreSQL solution for Database - Find loyal customers

--- database/medium/find_loyal_customers.sql
+++ database/medium/find_loyal_customers.sql
@@ -21,6 +21,29 @@ select distinct customer_id
 
 
 -- PostgreSQL
+-- Write your PostgreSQL query statement below
+with t (customer_id, transaction_date, transaction_type, n_purchases, n_days, n_refund_rate) as (
+    select customer_id, transaction_date, transaction_type,
+           sum(case when transaction_type = 'purchase' then 1 else 0 end) over (partition by customer_id) as n_purchases,
+           max(transaction_date) over (partition by customer_id)
+           -
+           min(transaction_date) over (partition by customer_id) as n_days,
+           sum(case when transaction_type = 'refund' then 1 else 0 end) over (partition by customer_id)
+           /
+           (count(*) over (partition by customer_id))::numeric as n_refund_rate
+      from customer_transactions
+)
+select distinct customer_id
+  from t
+ where n_purchases >= 3
+   and n_days >= 30
+   and n_refund_rate < 0.2
+ order by customer_id
+;
+
+
+
+
 
 
 -- SQL Server
