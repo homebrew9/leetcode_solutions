521871758319e61bedc23f1aa7824766c9882180
Pratyush Joshi <dmodeler@gmail.com>
2025-08-28 17:03:57 -0400

    Added solution for Gcd of odd and even sums

--- algorithms/easy/gcd_of_odd_and_even_sums.py
+++ algorithms/easy/gcd_of_odd_and_even_sums.py
@@ -4,6 +4,9 @@ class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
         return math.gcd(sum(range(1, 2*n+1, 2)), sum(range(2, 2*n+1, 2)))
 
+    def gcdOfOddEvenSums_1(self, n: int) -> int:
+        return n
+
 # Main section
 for n in [
             1,
@@ -15,9 +18,14 @@ for n in [
             159,
             876,
          ]:
-    print(f'n = {n}')
+    print(f'n  = {n}')
     sol = Solution()
     r = sol.gcdOfOddEvenSums(n)
-    print(f'r = {r}')
+    r1 = sol.gcdOfOddEvenSums(n)
+    print(f'r  = {r}')
+    print(f'r1 = {r}')
     print('===================')
+
+
+
 
