a69b1729c6bf031bd1467130bbc4ebc47ee1759a
Pratyush Joshi <dmodeler@gmail.com>
2025-08-28 17:06:42 -0400

    Added solution for Gcd of odd and even sums

--- algorithms/easy/gcd_of_odd_and_even_sums.py
+++ algorithms/easy/gcd_of_odd_and_even_sums.py
@@ -7,6 +7,20 @@ class Solution:
     def gcdOfOddEvenSums_1(self, n: int) -> int:
         return n
 
+    def gcdOfOddEvenSums_2(self, n: int) -> int:
+        # The trivial solution is to just return n.
+        # Make sure the proof for that is clear.
+        # This is a longer solution that does not use built-in math.gcd function.
+        def gcd_euclid(a, b):
+            if b == 0:
+                return a
+            if a >= b:
+                return gcd_euclid(b, a - b)
+            return gcd_euclid(a, b - a)
+        sum_odd = sum(range(1, 2*n + 1, 2))
+        sum_even = sum(range(2, 2*n + 1, 2))
+        return gcd_euclid(sum_odd, sum_even)
+
 # Main section
 for n in [
             1,
@@ -21,9 +35,14 @@ for n in [
     print(f'n  = {n}')
     sol = Solution()
     r = sol.gcdOfOddEvenSums(n)
-    r1 = sol.gcdOfOddEvenSums(n)
+    r1 = sol.gcdOfOddEvenSums_1(n)
+    r2 = sol.gcdOfOddEvenSums_2(n)
     print(f'r  = {r}')
-    print(f'r1 = {r}')
+    print(f'r1 = {r1}')
+    print(f'r2 = {r2}')
     print('===================')
+
+
+
 
 
