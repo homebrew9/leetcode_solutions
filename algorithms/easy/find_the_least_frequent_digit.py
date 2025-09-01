6a2ec2fa9c26d1f17664251b864139f80906d214
Pratyush Joshi <dmodeler@gmail.com>
2025-08-31 23:53:30 -0400

    Added solution for Find the least frequent digit

---
+++ algorithms/easy/find_the_least_frequent_digit.py
@@ -0,0 +1,43 @@
+from collections import Counter
+
+class Solution:
+    def getLeastFrequentDigit(self, n: int) -> int:
+        cntr = Counter(str(n))
+        min_freq = min(cntr.values())
+        return min([int(k) for k, v in cntr.items() if v == min_freq])
+
+# Main section
+for n in [
+            1553322,
+            723344511,
+            1583230031,
+            1307711736,
+            378387873,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.getLeastFrequentDigit(n)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
