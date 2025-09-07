534fa0d811b2c662eaf8798d644a78c4b7c6898f
Pratyush Joshi <dmodeler@gmail.com>
2025-09-06 23:25:24 -0400

    Added solution for Find N unique integers sum up to zero

--- algorithms/easy/find_n_unique_integers_sum_up_to_zero.py
+++ algorithms/easy/find_n_unique_integers_sum_up_to_zero.py
@@ -8,18 +8,58 @@ class Solution:
         if n % 2 == 1:
             arr += [0]
         return arr
-
+    def sumZero_1(self, n: int) -> List[int]:
+        is_odd = n % 2
+        res = list()
+        if is_odd:
+            res.append(0)
+            n -= 1
+        k = 1
+        for _ in range(0,n,2):
+            res.extend([-k, k])
+            k += 1
+        return res
+    def sumZero_2(self, n: int) -> List[int]:
+        return list(range(1,n)) + [n*(1-n)//2]
+ 
 # Main section
 for n in [
-            3,
             1,
+            2,
+            3,
+            4,
+            5,
             20,
             57,
-            129,
          ]:
     print(f'n = {n}')
     sol = Solution()
     r = sol.sumZero(n)
-    print(f'r = {r}')
+    r1 = sol.sumZero_1(n)
+    r2 = sol.sumZero_2(n)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
+    print(f'r2 = {r2}')
+    print(f'r : (len, sum) = ({len(r)}, {sum(r)}) \nr1: (len, sum) = ({len(r1)}, {sum(r1)}) \nr2: (len, sum) = ({len(r2)}, {sum(r2)})  ')
     print('===================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
