bff080f6fdc407f1620941b6c2be2c8a5edc40e0
Pratyush Joshi <dmodeler@gmail.com>
2025-09-13 23:54:47 -0400

    Added solution for Find most frequent vowel and consonant

---
+++ algorithms/easy/find_most_frequent_vowel_and_consonant.py
@@ -0,0 +1,49 @@
+from collections import Counter
+
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        c = Counter([ch for ch in s if ch not in 'aeiou']).most_common(1)
+        v = Counter([ch for ch in s if ch in 'aeiou']).most_common(1)
+        return (v[0][1] if v else 0) + (c[0][1] if c else 0)
+
+# Main section
+for s in [
+            'successes',
+            'aeiaeia',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.maxFreqSum(s)
+    print(f'r = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
