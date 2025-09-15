29b4669fc7ca8c93214a3f048057c856dd436c20
Pratyush Joshi <dmodeler@gmail.com>
2025-09-15 08:24:10 -0400

    Added solution for Check if a number is majority element in a sorted array

---
+++ algorithms/easy/check_if_a_number_is_majority_element_in_a_sorted_array.py
@@ -0,0 +1,55 @@
+from typing import List
+
+class Solution:
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+        N = len(nums)
+        # Leftmost index
+        left, right = 0, N - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if left < 0 or left >= N or nums[left] != target:
+            return False
+        leftmost_ind = left
+        # Rightmost index
+        left, right = 0, N - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if right < 0 or right >= N or nums[right] != target:
+            return False
+        rightmost_ind = right
+        occurrences = rightmost_ind - leftmost_ind + 1
+        return occurrences > N / 2
+
+# Main section
+for nums, target in [
+                       ([2,4,5,5,5,5,5,6,6], 5),
+                       ([10,100,101,101], 101),
+                    ]:
+    print(f'nums, target = {nums}, {target}')
+    sol = Solution()
+    r = sol.isMajorityElement(nums, target)
+    print(f'r = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
