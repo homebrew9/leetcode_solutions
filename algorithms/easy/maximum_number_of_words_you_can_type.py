b368369840f4d2d734f32e3e79549243a644f99a
Pratyush Joshi <dmodeler@gmail.com>
2025-09-20 23:37:51 -0400

    Added solution for Maximum number of words you can type

---
+++ algorithms/easy/maximum_number_of_words_you_can_type.py
@@ -0,0 +1,59 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        cnt = 0
+        for word in text.split():
+            st = set(word)
+            canType = True
+            for ch in st:
+                if ch in brokenLetters:
+                    canType = False
+                    break
+            if canType:
+                cnt += 1
+        return cnt
+
+# Main section
+for text, brokenLetters in [
+                              ('hello world', 'ad'),
+                              ('leet code', 'lt'),
+                              ('leet code', 'e'),
+                           ]:
+    print(f'text, brokenLetters = {text}, {brokenLetters}')
+    sol = Solution()
+    r = sol.canBeTypedWords(text, brokenLetters)
+    print(f'r = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
