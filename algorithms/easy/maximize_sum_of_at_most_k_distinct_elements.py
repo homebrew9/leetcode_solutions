dd52602be5f30a91951397e341e65bdc12db8a17
Pratyush Joshi <dmodeler@gmail.com>
2025-09-21 23:35:03 -0400

    Added solution for Maximize sum of at most k distinct elements

---
+++ algorithms/easy/maximize_sum_of_at_most_k_distinct_elements.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
+        return sorted(set(nums), reverse=True)[:k]
+
+# Main section
+for nums, k in [
+                  ([84,93,100,77,90], 3),
+                  ([84,93,100,77,93], 3),
+                  ([1,1,1,2,2,2], 6),
+               ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.maxKDistinct(nums, k)
+    print(f'r = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
