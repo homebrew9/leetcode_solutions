be67050b029175115e4ff10c984aa2a4ec401b58
Pratyush Joshi <dmodeler@gmail.com>
2025-09-21 23:29:06 -0400

    Added solution for Earliest time to finish one task

---
+++ algorithms/easy/earliest_time_to_finish_one_task.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def earliestTime(self, tasks: List[List[int]]) -> int:
+        return min([s + t for s, t in tasks])
+
+# Main section
+for tasks in [
+                [[1,6],[2,3]],
+                [[100,100],[100,100],[100,100]],
+             ]:
+    print(f'tasks = {tasks}')
+    sol = Solution()
+    r = sol.earliestTime(tasks)
+    print(f'r = {r}')
+    print('===================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
