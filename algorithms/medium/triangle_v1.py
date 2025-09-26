2a02754ea892ad73e0c7ee7f148247681240771c
Pratyush Joshi <dmodeler@gmail.com>
2025-09-25 23:55:56 -0400

    Added solution for Triangle, version 1

---
+++ algorithms/medium/triangle_v1.py
@@ -0,0 +1,58 @@
+from typing import List
+from functools import cache
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        @cache
+        def solve(r, c):
+            if r == rows:
+                return 0
+            return triangle[r][c] + min(solve(r+1, c), solve(r+1, c+1))
+        rows = len(triangle)
+        res = solve(0, 0)
+        return res
+
+# Main section
+for triangle in [
+                   [[2],[3,4],[6,5,7],[4,1,8,3]],
+                   [[-10]],
+                ]:
+    print(f'triangle = {triangle}')
+    sol = Solution()
+    r = sol.minimumTotal(triangle)
+    print(f'r = {r}')
+    print('==================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
