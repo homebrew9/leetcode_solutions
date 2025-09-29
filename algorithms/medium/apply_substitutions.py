2f68f8f5d451c6e24dbcef7d115787768fd19700
Pratyush Joshi <dmodeler@gmail.com>
2025-09-28 21:50:01 -0400

    Added solution for Apply substitutions

--- algorithms/medium/apply_substitutions.py
+++ algorithms/medium/apply_substitutions.py
@@ -1,5 +1,5 @@
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
@@ -30,6 +30,40 @@ class Solution:
             hsh[f'%{k}%'] = v
         res = solve(text)
         return res
+    def applySubstitutions_2(self, replacements: List[List[str]], text: str) -> str:
+        # Topological sort
+        hsh = defaultdict(str)
+        hsh1 = defaultdict(set)
+        for k, v in replacements:
+            key = f'%{k}%'
+            hsh[key] = v
+            hsh1[key] = set()
+            N = len(v)
+            i = 0
+            while i < N:
+                if v[i] == '%':
+                    hsh1[key].add(v[i:i+3])
+                    i += 3
+                    continue
+                i += 1
+        # We use Kahn's algorithm for Topological Sorting.
+        dq = deque()
+        key_list = list(hsh1.keys())
+        for k in key_list:
+            if len(hsh1[k]) == 0:
+                dq.append(k)
+                del hsh1[k]
+        while dq:
+            key = dq.popleft()
+            key_list = list(hsh1.keys())
+            for k in key_list:
+                if key in hsh1[k]:
+                    hsh[k] = hsh[k].replace(key, hsh[key])
+                    hsh1[k].remove(key)
+                    if len(hsh1[k]) == 0:
+                        dq.append(k)
+                        del hsh1[k]
+        return '_'.join([hsh[k] for k in text.split('_')])
 
 # Main section
 for replacements, text in [
@@ -42,8 +76,14 @@ for replacements, text in [
     sol = Solution()
     r = sol.applySubstitutions(replacements, text)
     r1 = sol.applySubstitutions_1(replacements, text)
+    r2 = sol.applySubstitutions_2(replacements, text)
     print(f'r  = {r}')
     print(f'r1 = {r1}')
-    assert(r == r1)
+    print(f'r2 = {r2}')
+    assert(r == r1 == r2)
     print('=============================')
+
+
+
+
 
