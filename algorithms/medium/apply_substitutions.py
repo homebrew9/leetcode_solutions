b3e6689bf77961c45f16f9791c39499073ddc774
Pratyush Joshi <dmodeler@gmail.com>
2025-09-28 21:47:43 -0400

    Added solution for Apply substitutions

--- algorithms/medium/apply_substitutions.py
+++ algorithms/medium/apply_substitutions.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 class Solution:
     def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
+        # Iterative solution
         hsh = defaultdict(str)
         for k, v in replacements:
             hsh[f'%{k}%'] = v
@@ -16,6 +17,19 @@ class Solution:
             if not found:
                 break
         return res
+    def applySubstitutions_1(self, replacements: List[List[str]], text: str) -> str:
+        # Recursive solution
+        def solve(s):
+            i = s.find('%')
+            if i >= 0:
+                k = s[i:i+3]
+                s = solve(s[:i] + hsh[k] + s[i+3:])
+            return s
+        hsh = defaultdict(str)
+        for k, v in replacements:
+            hsh[f'%{k}%'] = v
+        res = solve(text)
+        return res
 
 # Main section
 for replacements, text in [
@@ -27,8 +41,14 @@ for replacements, text in [
     print(f'text = {text}')
     sol = Solution()
     r = sol.applySubstitutions(replacements, text)
+    r1 = sol.applySubstitutions_1(replacements, text)
     print(f'r  = {r}')
+    print(f'r1 = {r1}')
+    assert(r == r1)
     print('=============================')
+
+
+
 
 
 
