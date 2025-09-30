e876ed1f060aaa61ee491a32809fe5e4f668865a
Pratyush Joshi <dmodeler@gmail.com>
2025-09-30 08:03:13 -0400

    Added solution for Minimum score triangulation of polygon

---
+++ algorithms/medium/minimum_score_triangulation_of_polygon.py
@@ -0,0 +1,59 @@
+from typing import List
+from functools import lru_cache
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        @lru_cache(None)
+        def dp(i, j):
+            if i + 2 > j:
+                return 0
+            if i + 2 == j:
+                return values[i] * values[i + 1] * values[j]
+            return min(
+                (values[i] * values[k] * values[j] + dp(i, k) + dp(k, j))
+                for k in range(i + 1, j)
+            )
+        return dp(0, len(values) - 1)
+
+# Main section
+for values in [
+                 [1,2,3],
+                 [3,7,4,5],
+                 [1,3,1,4,1,5],
+              ]:
+    print(f'values = {values}')
+    sol = Solution()
+    r = sol.minScoreTriangulation(values)
+    print(f'r = {r}')
+    print('=============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
