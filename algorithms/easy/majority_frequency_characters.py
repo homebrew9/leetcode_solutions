88f5fa6de4c8dc8e1eef1740399606f5af816439
Pratyush Joshi <dmodeler@gmail.com>
2025-10-03 23:08:03 -0400

    Added solution for Majority frequency characters

---
+++ algorithms/easy/majority_frequency_characters.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+class Solution:
+    def majorityFrequencyGroup(self, s: str) -> str:
+        hsh = defaultdict(int)
+        for ch in s:
+            hsh[ch] += 1
+        hsh1 = defaultdict(set)
+        for k, v in hsh.items():
+            hsh1[v].add(k)
+        arr = sorted([(list(v), k) for k, v in hsh1.items()], key=lambda x: (-len(x[0]), -x[1]))
+        return ''.join(arr[0][0])
+
+# Main section
+for s in [
+            'aaabbbccdddde',
+            'abcd',
+            'pfpfgi',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.majorityFrequencyGroup(s)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
