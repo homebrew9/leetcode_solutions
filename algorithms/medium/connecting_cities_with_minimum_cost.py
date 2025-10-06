79b227864e4b90d7f52a2b20a82b4e6e2b20674a
Pratyush Joshi <dmodeler@gmail.com>
2025-10-05 23:12:32 -0400

    Added solution for Connecting cities with minimum cost

---
+++ algorithms/medium/connecting_cities_with_minimum_cost.py
@@ -0,0 +1,58 @@
+import collections
+import heapq
+from typing import List
+
+class Solution:
+    '''Connecting Cities with Minimum Cost == Find Minimum Spanning Tree'''
+    def minimumCost(self, N: int, connections: List[List[int]]) -> int:
+        '''
+        Prim's Algorithm:
+        1) Initialize a tree with a single vertex, chosen
+        arbitrarily from the graph.
+        2) Grow the tree by one edge: of the edges that
+        connect the tree to vertices not yet in the tree,
+        find the minimum-weight edge, and transfer it to the tree.
+        3) Repeat step 2 (until all vertices are in the tree).
+        '''
+        # city1 <-> city2 may have multiple different cost connections,
+        # so use a list of tuples. Nested dict will break algorithm.
+        G = collections.defaultdict(list)
+        for city1, city2, cost in connections:
+            G[city1].append((cost, city2))
+            G[city2].append((cost, city1))
+        
+        queue = [(0, N)]  # [1] Arbitrary starting point N costs 0.
+        visited = set()
+        total = 0
+        while queue and len(visited) < N: # [3] Exit if all cities are visited.
+            # cost is always least cost connection in queue.
+            cost, city = heapq.heappop(queue)
+            if city not in visited:
+                visited.add(city)
+                total += cost # [2] Grow tree by one edge.
+                for edge_cost, next_city in G[city]:
+                    heapq.heappush(queue, (edge_cost, next_city))
+        return total if len(visited) == N else -1
+
+# Main section
+for N, connections in [
+                         (3, [[1,2,5],[1,3,6],[2,3,1]]),
+                         (4, [[1,2,3],[3,4,4]]),
+                      ]:
+    print(f'N, connections = {N}, {connections}')
+    sol = Solution()
+    r = sol.minimumCost(N, connections)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
