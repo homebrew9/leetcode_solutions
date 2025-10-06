28b9ee8fb2e580ab22e3ebbf6631cf285db3ec8f
Pratyush Joshi <dmodeler@gmail.com>
2025-10-05 23:07:04 -0400

    Added solution for Water bottles

--- algorithms/easy/water_bottles.py
+++ algorithms/easy/water_bottles.py
@@ -10,6 +10,19 @@ class Solution:
             numBottles = p + q
         return cnt
 
+    def numWaterBottles_1(self, numBottles: int, numExchange: int) -> int:
+        filled, empty = numBottles, 0
+        res = 0
+        while True:
+            # Drink the filled bottles
+            res += filled
+            empty += filled
+            # Now exchange the empty bottles if possible
+            if empty < numExchange:
+                break
+            filled, empty = divmod(empty, numExchange)
+        return res
+
 # Main section
 for numBottles, numExchange in [
                                   (9, 3),
@@ -18,6 +31,24 @@ for numBottles, numExchange in [
     print(f'numBottles, numExchange = {numBottles}, {numExchange}')
     sol = Solution()
     r = sol.numWaterBottles(numBottles, numExchange)
-    print(f'r = {r}')
+    r1 = sol.numWaterBottles_1(numBottles, numExchange)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
