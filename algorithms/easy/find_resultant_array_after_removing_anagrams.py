562b09a414a554793b4c654a1687c6d0cd0879ec
Pratyush Joshi <dmodeler@gmail.com>
2025-10-15 17:47:22 -0400

    Added solution for Find resultant array after removing anagrams

---
+++ algorithms/easy/find_resultant_array_after_removing_anagrams.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        N = len(words)
+        res = list()
+        res.append(words[0])
+        prev = sorted(words[0])
+        for i in range(1, N):
+            curr = sorted(words[i])
+            if curr != prev:
+                res.append(words[i])
+                prev = curr
+        return res
+
+# Main section
+for words in [
+                ['abba','baba','bbaa','cd','cd'],
+                ['a','b','c','d','e'],
+             ]:
+    print(f'words = {words}')
+    sol = Solution()
+    r = sol.removeAnagrams(words)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
