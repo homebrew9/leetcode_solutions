57932568bef15e68882ce8bb72928caddba23a9a
Pratyush Joshi <dmodeler@gmail.com>
2025-10-28 17:49:56 -0400

    Added solution for Next greater numerically balanced number

---
+++ algorithms/medium/next_greater_numerically_balanced_number.py
@@ -0,0 +1,55 @@
+from collections import Counter
+
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        i = n + 1
+        while i < 1224445:
+            count = Counter(str(i))
+            if all(count[d] == int(d) for d in count):
+                break
+            i += 1
+        return i
+
+# Main section
+for n in [
+            1,
+            1000,
+            3000,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.nextBeautifulNumber(n)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
