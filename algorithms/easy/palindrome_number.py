e71815e3a8079f51b99b82c4140902e813b16050
Pratyush Joshi <dmodeler@gmail.com>
2025-10-29 07:57:11 -0400

    Added solution for Palindrome number

---
+++ algorithms/easy/palindrome_number.py
@@ -0,0 +1,61 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        arr = list()
+        while x > 0:
+            q, r = divmod(x, 10)
+            arr.append(r)
+            x = q
+        return arr == arr[::-1]
+
+# Main section
+for x in [
+            121,
+            -121,
+            10,
+            0,
+         ]:
+    print(f'x = {x}')
+    sol = Solution()
+    r = sol.isPalindrome(x)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
