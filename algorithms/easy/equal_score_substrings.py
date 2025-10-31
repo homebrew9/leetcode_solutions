1f65c09fec23bf45e63928434edc842206d58cdc
Pratyush Joshi <dmodeler@gmail.com>
2025-10-30 23:45:15 -0400

    Added solution for Equal score substrings

---
+++ algorithms/easy/equal_score_substrings.py
@@ -0,0 +1,57 @@
+class Solution:
+    def scoreBalance(self, s: str) -> bool:
+        arr = [ord(ch) - ord('a') + 1 for ch in s]
+        total = sum(arr)
+        curr_sum = 0
+        for i in range(len(arr)-1):
+            curr_sum += arr[i]
+            if curr_sum == total - curr_sum:
+                return True
+        return False
+
+# Main section
+for s in [
+            'adcb',
+            'bace',
+            'vjtbbvkjvrkkngjldtvdlrgshdtfuccnuuttmfhhxzeuhurexcrdvxwlpgpzdwdksmudxngnrxbrarbfvhcteftkdbnerthgxpup',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.scoreBalance(s)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
