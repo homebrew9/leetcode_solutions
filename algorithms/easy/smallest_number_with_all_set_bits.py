9c2f28353587c8d62919bb6397db011b7c58e44a
Pratyush Joshi <dmodeler@gmail.com>
2025-11-01 11:52:51 -0400

    Added solution for Smallest number with all set bits

---
+++ algorithms/easy/smallest_number_with_all_set_bits.py
@@ -0,0 +1,25 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        p = 1           # Start with p = 2**0 = 1
+        while p <= n:
+            p = p << 1  # Replace p by the next power of 2, until p is strictly greater than n
+        return p - 1
+
+# Main section
+for n in [
+            5,
+            10,
+            3,
+            599,
+            103,
+            79,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.smallestNumber(n)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
