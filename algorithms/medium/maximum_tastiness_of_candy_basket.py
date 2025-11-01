c5a7802da8465f477890f2bc44585dfba662f573
Pratyush Joshi <dmodeler@gmail.com>
2025-11-01 11:48:16 -0400

    Added solution for Maximum tastiness of candy basket

---
+++ algorithms/medium/maximum_tastiness_of_candy_basket.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def maximumTastiness(self, price: List[int], k: int) -> int:
+        def solve(x):
+            cnt = 1
+            prev = price[0]
+            for i in range(1, N):
+                if price[i] - prev >= x:
+                    prev = price[i]
+                    cnt += 1
+                    if cnt >= k:
+                        break
+            return cnt >= k
+        N = len(price)
+        price.sort()
+        left, right = 0, max(price)
+        while left <= right:
+            mid = (left + right) // 2
+            is_valid = solve(mid)
+            if not is_valid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
+
+# Main section
+for price, k in [
+                   ([13,5,1,8,21,2], 3),
+                   ([1,3,1], 2),
+                   ([7,7,7,7], 2),
+                ]:
+    print(f'price, k = {price}, {k}')
+    sol = Solution()
+    r = sol.maximumTastiness(price, k)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
