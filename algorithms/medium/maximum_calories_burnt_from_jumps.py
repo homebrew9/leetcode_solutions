fa083e564ef47beac56d61898d4f4b0e38076fb5
Pratyush Joshi <dmodeler@gmail.com>
2025-11-04 18:08:13 -0500

    Added solution for Maximum calories burnt from jumps

---
+++ algorithms/medium/maximum_calories_burnt_from_jumps.py
@@ -0,0 +1,66 @@
+from typing import List
+
+class Solution:
+    def maxCaloriesBurnt(self, heights: List[int]) -> int:
+        N = len(heights)
+        heights.sort(reverse=True)
+        res = 0
+        prev = 0
+        left, right = 0, N - 1
+        while left < right:
+            res += (heights[left] - prev)**2
+            res += (heights[right] - heights[left])**2
+            prev = heights[right]
+            left += 1
+            right -= 1
+        if left == right:
+            res += (heights[left] - prev)**2
+        return res
+
+# Main section
+for heights in [
+                  [1,7,9],
+                  [5,2,4],
+                  [3,3],
+               ]:
+    print(f'heights = {heights}')
+    sol = Solution()
+    r = sol.maxCaloriesBurnt(heights)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
