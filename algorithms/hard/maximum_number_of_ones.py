417cd874c3d636e8652c83871a1d1967e2edafd4
Pratyush Joshi <dmodeler@gmail.com>
2025-11-05 21:46:12 -0500

    Added solution for Maximum number of ones

---
+++ algorithms/hard/maximum_number_of_ones.py
@@ -0,0 +1,53 @@
+class Solution:
+    def maximumNumberOfOnes(self, width: int, height: int, sideLength: int, maxOnes: int) -> int:
+        count = []
+        for r in range(sideLength):
+            for c in range(sideLength):
+                num = (1 + (width - c - 1) // sideLength) * (1 + (height - r - 1) // sideLength)
+                count.append(num)
+        count.sort(reverse=True)
+        return sum(count[:maxOnes])
+
+# Main section
+for width, height, sideLength, maxOnes in [
+                                             (3, 3, 2, 1),
+                                             (3, 3, 2, 2),
+                                          ]:
+    print(f'width, height, sideLength, maxOnes = {width}, {height}, {sideLength}, {maxOnes}')
+    sol = Solution()
+    r = sol.maximumNumberOfOnes(width, height, sideLength, maxOnes)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
