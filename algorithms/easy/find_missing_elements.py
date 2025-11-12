b750bbf26d40473193d3b4432f91db0de90d6200
Pratyush Joshi <dmodeler@gmail.com>
2025-11-11 23:09:24 -0500

    Added solution for Find missing elements

---
+++ algorithms/easy/find_missing_elements.py
@@ -0,0 +1,74 @@
+from typing import List
+
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        nums_set = set(nums)
+        res = list()
+        for i in range(min(nums), max(nums)+1):
+            if i not in nums_set:
+                res.append(i)
+        return res
+
+# Main section
+for nums in [
+               [1,4,2,5],
+               [7,8,6,9],
+               [5,1],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.findMissingElements(nums)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
