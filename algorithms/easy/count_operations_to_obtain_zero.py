b6794717ec328b9304e7a6c8a7127702e268897d
Pratyush Joshi <dmodeler@gmail.com>
2025-11-15 17:41:10 -0500

    Added solution for Count operations to obtain zero

---
+++ algorithms/easy/count_operations_to_obtain_zero.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        def solve(num1, num2):
+            if num1 == 0 or num2 == 0:
+                return 0
+            if num1 >= num2:
+                return 1 + solve(num1 - num2, num2)
+            return 1 + solve(num1, num2 - num1)
+        res = solve(num1, num2)
+        return res
+
+# Main section
+for num1, num2 in [
+                     (2, 3),
+                     (10, 10),
+                  ]:
+    print(f'num1, num2 = {num1}, {num2}')
+    sol = Solution()
+    r = sol.countOperations(num1, num2)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
