3353a3b2c6f6dafa2a40ef478a780d9d0f11e1a6
Pratyush Joshi <dmodeler@gmail.com>
2025-11-15 18:05:38 -0500

    Added solution for Minimum operations to convert all elements to zero

---
+++ algorithms/medium/minimum_operations_to_convert_all_elements_to_zero.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        s = []
+        res = 0
+        for a in nums:
+            while s and s[-1] > a:
+                s.pop()
+            if a == 0:
+                continue
+            if not s or s[-1] < a:
+                res += 1
+                s.append(a)
+        return res
+
+# Main section
+for nums in [
+               [0,2],
+               [3,1,2,1],
+               [1,2,1,2,1,2],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.minOperations(nums)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
