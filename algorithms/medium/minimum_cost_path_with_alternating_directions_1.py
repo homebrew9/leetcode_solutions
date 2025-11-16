28744ce39a653ad6dd53bdf5dc9e5f70c35241e3
Pratyush Joshi <dmodeler@gmail.com>
2025-11-16 11:17:15 -0500

    Added solution for Minimum cost path with alternating directions I

---
+++ algorithms/medium/minimum_cost_path_with_alternating_directions_1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minCost(self, m: int, n: int) -> int:
+        # After trying it out with pen and paper, it is easy to see that the moves
+        # cancel each other. Movement is possible only for trivial grids of sizes
+        # 1x1, 1x2, and 2x1.
+        if m == 1 and n == 1:
+            return 1
+        if (m == 1 and n == 2) or (m == 2 and n == 1):
+            return 3
+        return -1
+
+# Main section
+for m, n in [
+               (1, 1),
+               (2, 1),
+               (1, 2),
+               (1000, 1000),
+            ]:
+    print(f'm, n = {m}, {n}')
+    sol = Solution()
+    r = sol.minCost(m, n)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
