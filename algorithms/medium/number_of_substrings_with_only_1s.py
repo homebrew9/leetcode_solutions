01f69c6eeabf4c51db25ad8a4ddf338cbbc01458
Pratyush Joshi <dmodeler@gmail.com>
2025-11-17 23:54:55 -0500

    Added solution for Number of substrings with only 1s

--- algorithms/medium/number_of_substrings_with_only_1s.py
+++ algorithms/medium/number_of_substrings_with_only_1s.py
@@ -10,6 +10,27 @@ class Solution:
                 streak += 1
                 res += streak
         return res % MOD
+    def numSub_1(self, s: str) -> int:
+        # We can try using the formula for "sum of n"
+        MOD = 10**9 + 7
+        res = 0
+        N = len(s)
+        i, j = 0, 0
+        in_streak = False
+        while j < N:
+            if in_streak:
+                if s[j] == '0':
+                    streak = j - i
+                    res += streak * (streak + 1) // 2
+                    in_streak = False
+            elif s[j] == '1':
+                in_streak = True
+                i = j
+            j += 1
+        if in_streak:
+            streak = j - i
+            res += streak * (streak + 1) // 2
+        return res % MOD
 
 # Main section
 for s in [
@@ -20,7 +41,9 @@ for s in [
     print(f's = {s}')
     sol = Solution()
     r = sol.numSub(s)
-    print(f'r = {r}')
+    r1 = sol.numSub_1(s)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
     print('=====================')
 
 
