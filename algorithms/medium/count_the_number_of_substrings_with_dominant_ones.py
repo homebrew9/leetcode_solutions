c9af186fd01185ed65994065105116d4512306c6
Pratyush Joshi <dmodeler@gmail.com>
2025-11-20 22:55:47 -0500

    Added solution for Count the number of substrings with dominant ones

---
+++ algorithms/medium/count_the_number_of_substrings_with_dominant_ones.py
@@ -0,0 +1,81 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        n = len(s)
+        pre = [-1] * (n + 1)
+        for i in range(n):
+            if i == 0 or s[i - 1] == "0":
+                pre[i + 1] = i
+            else:
+                pre[i + 1] = pre[i]
+
+        res = 0
+        for i in range(1, n + 1):
+            cnt0 = 1 if s[i - 1] == "0" else 0
+            j = i
+            while j > 0 and cnt0 * cnt0 <= n:
+                cnt1 = (i - pre[j]) - cnt0
+                if cnt0 * cnt0 <= cnt1:
+                    res += min(j - pre[j], cnt1 - cnt0 * cnt0 + 1)
+                j = pre[j]
+                cnt0 += 1
+        return res
+
+# Main section
+for s in [
+            '00011',
+            '101101',
+            '1110100011',
+            '1001000001',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.numberOfSubstrings(s)
+    print(f'r = {r}')
+    print('=====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
