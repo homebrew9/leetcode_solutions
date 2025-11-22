524dd6d70decfd422a43c8f87e119ed9bbb17bda
Pratyush Joshi <dmodeler@gmail.com>
2025-11-21 23:21:10 -0500

    Added solution for Build an array with stack operations

---
+++ algorithms/medium/build_an_array_with_stack_operations.py
@@ -0,0 +1,40 @@
+from typing import List
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        max_val = max(target)
+        target_set = set(target)
+        res = list()
+        i = 1
+        while i <= max_val:
+            res.append('Push')
+            if i not in target_set:
+                res.append('Pop')
+            i += 1
+        return res
+
+# Main section
+for target, n in [
+                    ([1,3], 3),
+                    ([1,2,3], 3),
+                    ([1,2], 4),
+                 ]:
+    print(f'target, n = {target}, {n}')
+    sol = Solution()
+    r = sol.buildArray(target, n)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
