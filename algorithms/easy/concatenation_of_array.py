ec92dd477461704c42ae0772e5b8f4b90e5ef341
Pratyush Joshi <dmodeler@gmail.com>
2025-11-24 01:00:05 -0500

    Added solution for Concatenation of array

---
+++ algorithms/easy/concatenation_of_array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+
+# Main section
+for nums in [
+               [1,2,1],
+               [1,3,2,1],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.getConcatenation(nums)
+    print(f'r    = {r}')
+    print('===========================')
+
+
+
+
