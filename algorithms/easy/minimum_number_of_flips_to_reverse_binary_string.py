d18add9829c4849af16cb17a2329d770daa81d8a
Pratyush Joshi <dmodeler@gmail.com>
2025-11-25 23:57:45 -0500

    Added solution for Minimum number of flips to reverse binary string

---
+++ algorithms/easy/minimum_number_of_flips_to_reverse_binary_string.py
@@ -0,0 +1,58 @@
+class Solution:
+    def minimumFlips(self, n: int) -> int:
+        s = bin(n)[2:]
+        rev = s[::-1]
+        res = 0
+        for a, b in zip(s, rev):
+            if a != b:
+                res += 1
+        return res
+
+# Main section
+for n in [
+            7,
+            10,
+            12345,
+            293928328,
+            3599,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.minimumFlips(n)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
