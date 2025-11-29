7ecd6a7d6814f0be88f2ca753e463480db711771
Pratyush Joshi <dmodeler@gmail.com>
2025-11-29 14:59:53 -0500

    Added solution for Greatest sum divisible by three

---
+++ algorithms/medium/greatest_sum_divisible_by_three.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        dp = [0, float('-inf'), float('-inf')]
+        for num in nums:
+            new_dp = dp[:]  # Copy current state
+            for r in range(3):
+                new_r = (r + num) % 3
+                new_dp[new_r] = max(new_dp[new_r], dp[r] + num)
+            dp = new_dp
+        return dp[0]
+
+# Main section
+for nums in [
+               [3,6,5,1,8],
+               [4],
+               [1,2,3,4,4],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.maxSumDivThree(nums)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
