053e538067a8fd9c9c17a7b1acec2a5b38ce97f8
Pratyush Joshi <dmodeler@gmail.com>
2025-11-29 14:48:41 -0500

    Added solution for Concatenate non-zero digits and multiply by sum I

---
+++ algorithms/easy/concatenate_non_zero_digits_and_multiply_by_sum_1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        s = str(n)
+        total = 0
+        x = ''
+        for ch in s:
+            if ch != '0':
+                x += ch
+                total += int(ch)
+        if x == '':
+            return 0
+        return int(x) * total
+
+# Main section
+for n in [
+            10203004,
+            1000,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.sumAndMultiply(n)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
