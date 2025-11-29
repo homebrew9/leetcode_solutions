a4a560f8edee9731029a8b646487be5e505d5185
Pratyush Joshi <dmodeler@gmail.com>
2025-11-29 15:06:16 -0500

    Added solution for Pairs of songs with total durations divisible by 60

---
+++ algorithms/medium/pairs_of_songs_with_total_durations_divisible_by_60.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        count = [0] * 60
+        res = 0
+        for t in time:
+            r = t % 60
+            # Find complement remainder
+            complement = (60 - r) % 60
+            res += count[complement]
+            count[r] += 1
+        return res
+
+# Main section
+for time in [
+               [30,20,150,100,40],
+               [60,60,60],
+            ]:
+    print(f'time = {time}')
+    sol = Solution()
+    r = sol.numPairsDivisibleBy60(time)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
