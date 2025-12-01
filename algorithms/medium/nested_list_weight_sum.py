923cf9e1b12b997267204f5d02dd2ca6158c60ab
Pratyush Joshi <dmodeler@gmail.com>
2025-11-30 20:43:11 -0500

    Added solution for Nested list weight sum

--- algorithms/medium/nested_list_weight_sum.py
+++ algorithms/medium/nested_list_weight_sum.py
@@ -1,51 +1,51 @@
 from typing import List
 from collections import deque
 
-"""
-This is the interface that allows for creating nested lists.
-You should not implement it, or speculate about its implementation
-"""
-class NestedInteger:
-    def __init__(self, value=None):
-        """
-        If value is not specified, initializes an empty list.
-        Otherwise initializes a single integer equal to value.
-        """
-
-    def isInteger(self):
-        """
-        @return True if this NestedInteger holds a single integer, rather than a nested list.
-        :rtype bool
-        """
-
-    def add(self, elem):
-        """
-        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
-        :rtype void
-        """
-
-    def setInteger(self, value):
-        """
-        Set this NestedInteger to hold a single integer equal to value.
-        :rtype void
-        """
-
-    def getInteger(self):
-        """
-        @return the single integer that this NestedInteger holds, if it holds a single integer
-        Return None if this NestedInteger holds a nested list
-        :rtype int
-        """
-
-    def getList(self):
-        """
-        @return the nested list that this NestedInteger holds, if it holds a nested list
-        Return None if this NestedInteger holds a single integer
-        :rtype List[NestedInteger]
-        """
+#"""
+#This is the interface that allows for creating nested lists.
+#You should not implement it, or speculate about its implementation
+#"""
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
 
 class Solution:
-    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int: # type: ignore
         # We use BFS here. If we encounter a list at a particular
         # level, then we append it to the dq so that it could be
         # processed at the next level iteration.
