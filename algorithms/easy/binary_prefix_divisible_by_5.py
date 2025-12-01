ad8e89ed4fa424e970d6b34a2c4ea17edc7b265e
Pratyush Joshi <dmodeler@gmail.com>
2025-11-30 20:45:51 -0500

    Added solution for Binary prefix divisible by 5

---
+++ algorithms/easy/binary_prefix_divisible_by_5.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        x = 0
+        res = list()
+        for n in nums:
+            x = (x * 2 + n) % 5
+            res.append(x == 0)
+        return res
+
+# Main section
+for nums in [
+               [0,1,1],
+               [1,1,1],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.prefixesDivBy5(nums)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
