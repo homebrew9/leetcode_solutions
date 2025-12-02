523f40589f10fa1139b30df28f46dbee63a0600c
Pratyush Joshi <dmodeler@gmail.com>
2025-12-02 07:40:01 -0500

    Added solution for Super pow

---
+++ algorithms/medium/super_pow.py
@@ -0,0 +1,49 @@
+from typing import List
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        res = 1
+        for d in b:
+            res = ((res**10 % 1337) * (a**d % 1337)) % 1337
+        return res
+
+# Main section
+for a, b in [
+               (2, [3]),
+               (2, [1,0]),
+               (1, [4,3,3,8,5,2]),
+            ]:
+    print(f'a, b = {a}, {b}')
+    sol = Solution()
+    r = sol.superPow(a, b)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
