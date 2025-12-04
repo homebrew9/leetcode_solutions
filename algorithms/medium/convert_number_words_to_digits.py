c5d65f51cf46bada758cee6148a6cb86487f0f46
Pratyush Joshi <dmodeler@gmail.com>
2025-12-03 23:34:44 -0500

    Added solution for Convert number words to digits

---
+++ algorithms/medium/convert_number_words_to_digits.py
@@ -0,0 +1,74 @@
+class Solution:
+    def convertNumber(self, s: str) -> str:
+        # One optimization trick is to check string chunks of lengths 5, 4, and 3 in that order.
+        hsh = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+               'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+              }
+        res = ''
+        N = len(s)
+        i = 0
+        while i < N:
+            if s[i:i+5] in hsh:
+                res += hsh[s[i:i+5]]
+                i += 5
+            elif s[i:i+4] in hsh:
+                res += hsh[s[i:i+4]]
+                i += 4
+            elif s[i:i+3] in hsh:
+                res += hsh[s[i:i+3]]
+                i += 3
+            else:
+                i += 1
+        return res
+
+# Main section
+for s in [
+            'onefourthree',
+            'ninexsix',
+            'zeero',
+            'tw',
+            'zero',
+            'ffour',
+            'jfone',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.convertNumber(s)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
