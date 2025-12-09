25c9308498627b551ff2ee06256aaa25df346d92
Pratyush Joshi <dmodeler@gmail.com>
2025-12-09 18:01:54 -0500

    Added solution for Maximum running time of N computers

---
+++ algorithms/hard/maximum_running_time_of_n_computers.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        left, right = 1, sum(batteries) // n
+        
+        while left < right:
+            target = right - (right - left) // 2
+            
+            extra = 0
+            for power in batteries:
+                extra += min(power, target)
+            
+            if extra // n >= target:
+                left = target
+            else:
+                right = target - 1
+        
+        return left
+
+# Main section
+for n, batteries in [
+                       (2, [3,3,3]),
+                       (2, [1,1,1,1]),
+                    ]:
+    print(f'n, batteries = {n}, {batteries}')
+    sol = Solution()
+    r = sol.maxRunTime(n, batteries)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
