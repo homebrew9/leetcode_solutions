2d555c39d2c42f502e3cc81c523bf4aae8a6ac9d
Pratyush Joshi <dmodeler@gmail.com>
2025-12-11 22:45:24 -0500

    Added solution for Maximum number of equal length runs

---
+++ algorithms/medium/maximum_number_of_equal_length_runs.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+
+class Solution:
+    def maxSameLengthRuns(self, s: str) -> int:
+        hsh = defaultdict(int)
+        N = len(s)
+        i, j = 0, 0
+        while j < N:
+            if s[i] == s[j]:
+                j += 1
+            else:
+                hsh[j - i] += 1
+                i = j
+        hsh[j - i] += 1
+        return max(hsh.values())
+
+# Main section
+for s in [
+            'hello',
+            'aaabaaa',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.maxSameLengthRuns(s)
+    print(f'r = {r}')
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
