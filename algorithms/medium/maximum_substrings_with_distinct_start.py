2b6230eb0c57f42c9839158634f2af42c4c87cd9
Pratyush Joshi <dmodeler@gmail.com>
2025-12-15 22:36:44 -0500

    Added solution for Maximum substrings with distinct start

---
+++ algorithms/medium/maximum_substrings_with_distinct_start.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxDistinct(self, s: str) -> int:
+        seen = set()
+        res = 0
+        for ch in s:
+            if ch not in seen:
+                res += 1
+                seen.add(ch)
+        return res
+    def maxDistinct_1(self, s: str) -> int:
+        return len(set(s))
+
+# Main section
+for s in [
+            'abab',
+            'abcd',
+            'aaaa',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.maxDistinct(s)
+    r1 = sol.maxDistinct(s)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
+    assert(r == r1)
+    print('===========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
