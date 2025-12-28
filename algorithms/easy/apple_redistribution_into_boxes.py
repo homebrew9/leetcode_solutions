038731be8205fa81f5b2857f9e0405419b6d26d1
Pratyush Joshi <dmodeler@gmail.com>
2025-12-28 11:28:14 -0500

    Added solution for Apple redistribution into boxes

---
+++ algorithms/easy/apple_redistribution_into_boxes.py
@@ -0,0 +1,30 @@
+from typing import List
+from itertools import accumulate
+
+class Solution:
+    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+        total = sum(apple)
+        arr = list(accumulate(sorted(capacity, reverse=True)))
+        return sum([x < total for x in arr]) + 1
+
+# Main section
+for apple, capacity in [
+                          ([1,3,2], [4,3,1,5,2]),
+                          ([5,5,5], [2,4,2,7]),
+                       ]:
+    print(f'apple, capacity = {apple}, {capacity}')
+    sol = Solution()
+    r = sol.minimumBoxes(apple, capacity)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
