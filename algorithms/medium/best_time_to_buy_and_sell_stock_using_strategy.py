c27d8750bfb125414be90368a6fac81bf99b99c8
Pratyush Joshi <dmodeler@gmail.com>
2025-12-27 20:50:03 -0500

    Added solution for Best time to buy and sell stock using strategy

---
+++ algorithms/medium/best_time_to_buy_and_sell_stock_using_strategy.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
+        n = len(prices)
+        profitSum = [0] * (n + 1)
+        priceSum = [0] * (n + 1)
+        for i in range(n):
+            profitSum[i + 1] = profitSum[i] + prices[i] * strategy[i]
+            priceSum[i + 1] = priceSum[i] + prices[i]
+        res = profitSum[n]
+        for i in range(k - 1, n):
+            leftProfit = profitSum[i - k + 1]
+            rightProfit = profitSum[n] - profitSum[i + 1]
+            changeProfit = priceSum[i + 1] - priceSum[i - k // 2 + 1]
+            res = max(res, leftProfit + changeProfit + rightProfit)
+        return res
+
+# Main section
+for prices, strategy, k in [
+                              ([4,2,8], [-1,0,1], 2),
+                              ([5,4,3], [1,1,0], 2),
+                           ]:
+    print(f'prices, strategy, k = {prices}, {strategy}, {k}')
+    sol = Solution()
+    r = sol.maxProfit(prices, strategy, k)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
