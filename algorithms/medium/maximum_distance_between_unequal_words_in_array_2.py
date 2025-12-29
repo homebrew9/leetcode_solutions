8a415c660aada32d552309352bdb538f71cef04c
Pratyush Joshi <dmodeler@gmail.com>
2025-12-29 12:26:47 -0500

    Added solution for Maximum distance between unequal words in array II

---
+++ algorithms/medium/maximum_distance_between_unequal_words_in_array_2.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def maxDistance(self, words: List[str]) -> int:
+        N = len(words)
+        for i in range(N):
+            if words[i] != words[N - 1]:
+                return max(N - i, i + 1)
+        return 0
+
+# Main section
+for words in [
+                ['leetcode','leetcode','codeforces'],
+                ['a','b','c','a','a'],
+                ['z','z','z'],
+             ]:
+    print(f'words = {words}')
+    sol = Solution()
+    r = sol.maxDistance(words)
+    print(f'r = {r}')
+    print('====================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
