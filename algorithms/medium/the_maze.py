323b62abe1586e8b4229964d2e338693e5533882
Pratyush Joshi <dmodeler@gmail.com>
2026-01-15 10:31:04 -0500

    Added solution for The maze

---
+++ algorithms/medium/the_maze.py
@@ -0,0 +1,45 @@
+from typing import List
+
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        m, n, stopped = len(maze), len(maze[0]), set()
+        def dfs(x, y):
+            if (x, y) in stopped: 
+                return False
+            stopped.add((x, y))
+            if [x, y] == destination:
+                return True
+            for i, j in (-1, 0) , (1, 0), (0, -1), (0, 1):
+                newX, newY = x, y
+                while 0 <= newX + i < m and 0 <= newY + j < n and maze[newX + i][newY + j] != 1:
+                    newX += i
+                    newY += j
+                if dfs(newX, newY):
+                    return True
+            return False
+        return dfs(*start)
+
+# Main section
+for maze, start, destination in [
+                                   ([[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], [0,4], [4,4]),
+                                   ([[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], [0,4], [3,2]),
+                                   ([[0,0,0,0,0],[1,1,0,0,1],[0,0,0,0,0],[0,1,0,0,1],[0,1,0,0,0]], [4,3], [0,1]),
+                                ]:
+    print(f'maze, start, destination = {maze}, {start}, {destination}')
+    sol = Solution()
+    r = sol.hasPath(maze, start, destination)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
+
+
