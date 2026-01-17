60394440a57660d52f5fde710ad23e2cf8a82bd7
Pratyush Joshi <dmodeler@gmail.com>
2026-01-17 10:41:30 -0500

    Added solution for First letter to appear twice

---
+++ algorithms/easy/first_letter_to_appear_twice.py
@@ -0,0 +1,32 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = set()
+        res = ''
+        for ch in s:
+            if ch in seen:
+                res = ch
+                break
+            seen.add(ch)
+        return res
+
+# Main section
+for s in [
+            'abccbaacz',
+            'abcdd',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.repeatedCharacter(s)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
