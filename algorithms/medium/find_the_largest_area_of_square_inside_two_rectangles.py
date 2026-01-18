c191188e2522308b40e126be6bea7a2d3ab07ad3
Pratyush Joshi <dmodeler@gmail.com>
2026-01-18 10:52:21 -0500

    Added solution for Find the largest area of square inside two rectangles

---
+++ algorithms/medium/find_the_largest_area_of_square_inside_two_rectangles.py
@@ -0,0 +1,37 @@
+from typing import List
+from itertools import combinations
+
+class Solution:
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        max_size = 0
+
+        for (bottom_left_i, top_right_i), (bottom_left_j, top_right_j) in combinations(zip(bottomLeft, topRight), 2):
+            w = min(top_right_i[0], top_right_j[0]) - max(bottom_left_i[0], bottom_left_j[0])
+            h = min(top_right_i[1], top_right_j[1]) - max(bottom_left_i[1], bottom_left_j[1])
+            max_size = max(max_size, min(w, h))
+
+        return max_size * max_size
+
+# Main section
+for bottomLeft, topRight in [
+                               ([[1,1],[2,2],[3,1]], [[3,3],[4,4],[6,6]]),
+                               ([[1,1],[1,3],[1,5]], [[5,5],[5,7],[5,9]]),
+                               ([[1,1],[2,2],[1,2]], [[3,3],[4,4],[3,4]]),
+                               ([[1,1],[3,3],[3,1]], [[2,2],[4,4],[4,2]]),
+                            ]:
+    print(f'bottomLeft, topRight = {bottomLeft}, {topRight}')
+    sol = Solution()
+    r = sol.largestSquareArea(bottomLeft, topRight)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
