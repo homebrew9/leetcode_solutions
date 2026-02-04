30cc62d61b12ebbe8c3fe8f3f73261c178eba5ce
Pratyush Joshi <dmodeler@gmail.com>
2026-02-03 23:19:37 -0500

    Added solution for Trionic array II

---
+++ algorithms/hard/trionic_array_2.py
@@ -0,0 +1,70 @@
+from typing import List
+
+class Solution:
+    def maxSumTrionic(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = -10**20
+        i = 0
+        while i < n:
+            j = i + 1
+            res = 0
+            # first segment: increasing segment
+            while j < n and nums[j - 1] < nums[j]:
+                j += 1
+            p = j - 1
+            if p == i:  # there is no valid increasing segment
+                i += 1
+                continue
+            # second segment: decreasing segment
+            res += nums[p] + nums[p - 1]
+            while j < n and nums[j - 1] > nums[j]:
+                res += nums[j]
+                j += 1
+            q = j - 1
+            if q == p or q == n - 1 or (j < n and nums[j] <= nums[q]):
+                i = q
+                continue
+            # third segment: increasing segment
+            res += nums[q + 1]
+            # find the maximum sum of the third segment
+            max_sum = 0
+            curr_sum = 0
+            k = q + 2
+            while k < n and nums[k] > nums[k - 1]:
+                curr_sum += nums[k]
+                max_sum = max(max_sum, curr_sum)
+                k += 1
+            res += max_sum
+            # find the maximum sum of the first segment
+            max_sum = 0
+            curr_sum = 0
+            for k in range(p - 2, i - 1, -1):
+                curr_sum += nums[k]
+                max_sum = max(max_sum, curr_sum)
+            res += max_sum
+            # update answer
+            ans = max(ans, res)
+            i = q
+        return ans
+
+# Main section
+for nums in [
+               [0,-2,-1,-3,0,2,-1],
+               [1,4,2,7],
+            ]:
+    print(f'nums = {nums}')
+    sol = Solution()
+    r = sol.maxSumTrionic(nums)
+    print(f'r = {r}')
+    print('==========================')
+
+
+
+
+
+
+
+
+
+
+
