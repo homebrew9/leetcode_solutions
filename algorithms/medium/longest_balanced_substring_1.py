cbf6b7acd47c8385cb33c0f25020b36438ebf6a2
Pratyush Joshi <dmodeler@gmail.com>
2026-02-11 22:08:46 -0500

    Added solution for Longest balanced substring I

---
+++ algorithms/medium/longest_balanced_substring_1.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        N = len(s)
+        res = 0
+        for i in range(N):
+            hsh = defaultdict(int)
+            for j in range(i, N):
+                hsh[s[j]] += 1
+                if len(set(hsh.values())) == 1:
+                    res = max(res, j - i + 1)
+        return res
+
+# Main section
+for s in [
+            'abbac',
+            'zzabccy',
+            'aba',
+            'x',
+            'aa',
+            'ab',
+            'xyx',
+            'brvlkzweeekxgjrcnmelakbfxtezecywcxpygbrdjsskeqqqduysenwjlwurayamkduxvwnfltvqmqbfgfgqsxrgxirtexjysvpzqpcvpkmuqtptruyhjttzwbckwiyhqy',
+         ]:
+    print(f's = {s}')
+    sol = Solution()
+    r = sol.longestBalanced(s)
+    print(f'r = {r}')
+    print('=================================================')
+ 
+
+
+
+
+
+
+
+
+
+
+
