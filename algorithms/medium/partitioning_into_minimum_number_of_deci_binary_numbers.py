592459dadff621bfe926f8603ae063b37e7f1780
Pratyush Joshi <dmodeler@gmail.com>
2026-03-01 19:56:28 -0500

    Added solution for Partitioning into minimum number of deci-binary numbers

---
+++ algorithms/medium/partitioning_into_minimum_number_of_deci_binary_numbers.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(list(n)))
+
+# Main section
+for n in [
+            '32',
+            '82734',
+            '27346209830709182346',
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.minPartitions(n)
+    print(f'r = {r}')
+    print('=================================================')
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
