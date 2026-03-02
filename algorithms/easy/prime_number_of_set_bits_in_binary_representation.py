860ba77acad0675ad860d45f22f491774cb57cd6
Pratyush Joshi <dmodeler@gmail.com>
2026-03-01 19:52:33 -0500

    Added solution for Prime number of set bits in binary representation

---
+++ algorithms/easy/prime_number_of_set_bits_in_binary_representation.py
@@ -0,0 +1,46 @@
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        # Upper limit 10^6 has 20 bits
+        primes = {2,3,5,7,11,13,17,19}
+        res = 0
+        for n in range(left, right+1):
+            if bin(n)[2:].count('1') in primes:
+                res += 1
+        return res
+
+# Main section
+for left, right in [
+                      (6, 10),
+                      (10, 15),
+                   ]:
+    print(f'left, right = {left}, {right}')
+    sol = Solution()
+    r = sol.countPrimeSetBits(left, right)
+    print(f'r = {r}')
+    print('=================================================')
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
