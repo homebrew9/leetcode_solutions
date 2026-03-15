37714813024f48e9669de52e3dbf78f939446c22
Pratyush Joshi <dmodeler@gmail.com>
2026-03-15 18:56:02 -0400

    Added solution for Count commas in range

---
+++ algorithms/easy/count_commas_in_range.py
@@ -0,0 +1,47 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        res = 0
+        for i in range(1, n+1):
+            if i >= 1000:
+                res += 1
+        return res
+    def countCommas_1(self, n: int) -> int:
+        return max(0, n - 999)
+
+# Main section
+for n in [
+            1002,
+            998,
+            1,
+            10000,
+            100000,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.countCommas(n)
+    r1 = sol.countCommas_1(n)
+    print(f'r  = {r}')
+    print(f'r1 = {r1}')
+    assert(r == r1)
+    print('===============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
