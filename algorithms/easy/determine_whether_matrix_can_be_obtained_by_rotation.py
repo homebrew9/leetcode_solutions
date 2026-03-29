3209e2ceec839357ca333b1175e1929449600c0e
Pratyush Joshi <dmodeler@gmail.com>
2026-03-29 13:11:21 -0400

    Added solution for Determine whether matrix can be obtained by rotation

---
+++ algorithms/easy/determine_whether_matrix_can_be_obtained_by_rotation.py
@@ -0,0 +1,40 @@
+from typing import List
+
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        def rotate(mat):
+            N = len(mat)
+            grid = [[-1 for _ in range(N)] for _ in range(N)]
+            for r in range(N):
+                row = 0
+                col = N - r - 1
+                for c in range(N):
+                    grid[row][col] = mat[r][c]
+                    row += 1
+            return grid
+        
+        if mat == target:
+            return True
+        for _ in range(3):
+            mat = rotate(mat)
+            if mat == target:
+                return True
+        return False
+
+# Main section
+for mat, target in [
+                      ([[0,1],[1,0]], [[1,0],[0,1]]),
+                      ([[0,1],[1,1]], [[1,0],[0,1]]),
+                      ([[0,0,0],[0,1,0],[1,1,1]], [[1,1,1],[0,1,0],[0,0,0]]),
+                   ]:
+    print(f'mat, target = {mat}, {target}')
+    sol = Solution()
+    r = sol.findRotation(mat, target)
+    print(f'r = {r}')
+    print('===============================')
+
+
+
+
+
+
