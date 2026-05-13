7d35317886259319d8563d5a096955e171e097a5
Pratyush Joshi <dmodeler@gmail.com>
2026-05-12 22:45:09 -0400

    Added solution for Robot return to origin

---
+++ algorithms/easy/robot_return_to_origin.py
@@ -0,0 +1,32 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        up, down, left, right = 0, 0, 0, 0
+        for m in moves:
+            if m == 'U': up += 1
+            elif m == 'D': down += 1
+            elif m == 'L': left += 1
+            elif m == 'R': right += 1
+        return up == down and left == right
+
+# Main section
+for moves in [
+                'UD',
+                'LL',
+                'UDDUURLRLLRRUDUDLLRLURLRLRLUUDLULRULRLDDDUDDDDLRRDDRDRLRLURRLLRUDURULULRDRDLURLUDRRLRLDDLUUULUDUUUUL',
+             ]:
+    print(f'moves = {moves}')
+    sol = Solution()
+    r = sol.judgeCircle(moves)
+    print(f'r = {r}')
+    print('==================================')
+
+
+
+
+
+
+
+
+
+
+
