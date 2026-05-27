58249deb3bf31aa2362e9d5fdc705bd5e8b5e18c
Pratyush Joshi <dmodeler@gmail.com>
2026-05-27 15:13:58 -0400

    Added solution for Count the number of special characters I

---
+++ algorithms/easy/count_the_number_of_special_characters_1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        res = 0
+        seen = set()
+        for ch in word:
+            if ch not in seen:
+                if (ch.islower() and ch.upper() in seen) or (ch.isupper() and ch.lower() in seen):
+                    res += 1
+            seen.add(ch)
+        return res
+
+# Main section
+for word in [
+               'aaAbcBC',
+               'abc',
+               'abBCab',
+               'abBCabABacBCAa',
+            ]:
+    print(f'word = {word}')
+    sol = Solution()
+    r = sol.numberOfSpecialChars(word)
+    print(f'r = {r}')
+    print('==================================')
+
+
+
+
+
+
+
+
+
+
+
