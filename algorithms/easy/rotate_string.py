a6d2f0477bdbf8f27c35674b281a337bb39a4c6e
Pratyush Joshi <dmodeler@gmail.com>
2026-06-04 16:11:20 -0400

    Added solution for Rotate string

---
+++ algorithms/easy/rotate_string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return any([s[i:]+s[:i] == goal for i in range(len(s))])
+
+# Main section
+for s, goal in [
+                  ('abcde', 'cdeab'),
+                  ('abcde', 'abced'),
+               ]:
+    print(f's, goal = {s}, {goal}')
+    sol = Solution()
+    r = sol.rotateString(s, goal)
+    print(f'r = {r}')
+    print('==============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
