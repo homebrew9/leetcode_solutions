e436796f3e39c55c6e10d6fd1d14a5af643f9ad4
Pratyush Joshi <dmodeler@gmail.com>
2026-06-06 16:31:37 -0400

    Added solution for Exactly one consecutive set bits pair

---
+++ algorithms/easy/exactly_one_consecutive_set_bits_pair.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+class Solution:
+    def consecutiveSetBits(self, n: int) -> bool:
+        hsh = defaultdict(int)
+        streak = 0
+        for b in bin(n)[2:]:
+            if b == '1':
+                streak += 1
+            else:
+                if streak > 0:
+                    hsh[streak] += 1
+                streak = 0
+        if streak > 0:
+            hsh[streak] += 1
+        for k, v in hsh.items():
+            if k > 2:
+                return False
+        return 2 in hsh and hsh[2] == 1
+
+# Main section
+for n in [
+            3,
+            7,
+            12,
+            45,
+            35,
+            111,
+            15,
+            31,
+         ]:
+    print(f'n = {n}')
+    sol = Solution()
+    r = sol.consecutiveSetBits(n)
+    print(f'r = {r}')
+    print('==============================')
+
+
+
+
+
+
+
+
+
+
+
