c8f163b57511bfc9d0acfec09f8620da45b689c0
Pratyush Joshi <dmodeler@gmail.com>
2026-06-07 14:24:47 -0400

    Added solution for Sum of compatible numbers in range I

---
+++ algorithms/easy/sum_of_compatible_numbers_in_range_1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def sumOfGoodIntegers(self, n: int, k: int) -> int:
+        res = 0
+        for x in range(max(1, n-k), n+k+1):
+            if (n & x) == 0:
+                res += x
+        return res
+
+# Main section
+for n, k in [
+               (2, 3),
+               (5, 1),
+               (4, 9),
+               (17, 83),
+            ]:
+    print(f'n, k = {n}, {k}')
+    sol = Solution()
+    r = sol.sumOfGoodIntegers(n, k)
+    print(f'r = {r}')
+    print('==============================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
