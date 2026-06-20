5d7ee50fedbc0ce4dd4cc78d849b8d1bc00da455
Pratyush Joshi <dmodeler@gmail.com>
2026-06-20 19:16:54 -0400

    Added solution for Create grid with exactly one path

---
+++ algorithms/easy/create_grid_with_exactly_one_path.py
@@ -0,0 +1,38 @@
+class Solution:
+    def createGrid(self, m: int, n: int) -> list[str]:
+        rows, cols = m, n
+        res = list()
+        res.append('.' * cols)
+        for r in range(1, rows):
+            tmp = '#' * (cols - 1) + '.'
+            res.append(tmp)
+        return res
+
+# Main section
+for m, n in [
+               (2, 3),
+               (3, 3),
+               (1, 4),
+               (5, 1),
+            ]:
+    print(f'm, n = {m}, {n}')
+    sol = Solution()
+    r = sol.createGrid(m, n)
+    print(f'r = {r}')
+    print('===================================')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
